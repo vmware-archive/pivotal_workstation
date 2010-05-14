60fed2ede92dda92bd6f203c139299dd295571b0
Pivotal Labs <pivotal@dyn-172-17-3-138.flood.pivotallabs.com>
2010-05-14 13:49:19 -0700

    initial commit

---
+++ Gemfile
@@ -0,0 +1,2 @@
+source "http://rubygems.org"
+gem "chef", "0.8.14"
---
+++ install_chef.rb
@@ -0,0 +1,15 @@
+#assume that we're in the project directory
+require "fileutils"
+
+raise "don't run this using sudo" if ENV["USER"] == "root"
+
+module Kernel
+  def system!(cmd)
+    system(cmd) || raise("FAILED: #{cmd}")
+  end
+end
+
+system!("sudo sh -c 'gem update --system && gem install bundler -v 0.9 --no-rdoc --no-ri'")
+
+FileUtils.mkdir_p("vendor/bundle")
+system!("bundle install vendor/bundle")
