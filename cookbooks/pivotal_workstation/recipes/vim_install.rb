df38014730975b9d18ee7fb969a247dac0b09ace
Pivotal Labs <pivotal@Pivotal-Labss-iMac.local>
2010-07-12 18:35:15 -0700

    vim is broken, fix it

--- cookbooks/pivotal_workstation/recipes/vim_install.rb
+++ cookbooks/pivotal_workstation/recipes/vim_install.rb
@@ -2,13 +2,19 @@ vim_version  = version_string_for("vim")
 ::VIM_HOME = "#{WS_HOME}/.vim"
 
 run_unless_marker_file_exists(marker_version_string_for("vim")) do
+  directory VIM_HOME do
+    owner WS_USER
+  end
+  
   execute "get vim source" do
-    command 'curl "ftp://ftp.vim.org/pub/vim/unix/vim-#{vim_version}.tar.bz2" | bunzip2 | tar -xf - -C #{VIM_HOME}'
+    cmd = "curl ftp://ftp.vim.org/pub/vim/unix/vim-#{vim_version}.tar.bz2 | bunzip2 | tar -xf - -C #{VIM_HOME}"
+    puts cmd
+    command cmd
     user WS_USER
   end
 
   execute "install vim" do
-    command "cd #{VIM_HOME} && ./configure --with-features=huge --enable-rubyinterp && make && make install"
+    command "cd #{VIM_HOME}/vim72 && ./configure --with-features=huge --enable-rubyinterp && make && make install"
     user WS_USER
   end
 
