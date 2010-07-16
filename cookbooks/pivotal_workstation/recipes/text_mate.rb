707d87560b69a0f088c3ed03a27ccae510e451db
pivotal <pivotal@dyn-172-17-3-55.flood.pivotallabs.com>
2010-07-16 08:53:50 -0700

    adding an experimental textmate recipe, and adding a simple recipe to make the workspace directory.

---
+++ cookbooks/pivotal_workstation/recipes/text_mate.rb
@@ -0,0 +1,23 @@
+##  This is very experimental and unsupported.
+
+run_unless_marker_file_exists("text_mate_1_5_9") do
+  execute "download text mate to temp dir" do
+    command "curl -o /tmp/textmate.dmg http://download.macromates.com/TextMate_1.5.9.dmg"
+    user WS_USER
+  end
+  
+  execute "mount text mate dmg" do
+    command "hdid /tmp/textmate.dmg"
+    user WS_USER
+  end
+  
+  execute "copy text mate to /Applications" do
+    command 'cp -r /Volumes/TextMate\\ 1.5.9/TextMate.app /Applications/'
+    user WS_USER
+  end
+  
+  execute "unmount dmg" do
+    command "hdiutil detach  /Volumes/TextMate\\ 1.5.9/"
+    user WS_USER
+  end
+end
---
+++ cookbooks/pivotal_workstation/recipes/workspace_directory.rb
@@ -0,0 +1,5 @@
+directory "#{WS_HOME}/Workspace" do
+  owner WS_USER
+  mode "0755"
+  action :create
+end
