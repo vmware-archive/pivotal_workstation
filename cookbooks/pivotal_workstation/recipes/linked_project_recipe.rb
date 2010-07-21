0405f552a46e56b81d62b4762e6d1b1e161bc7f6
Matthew Kocher <kocher@gmail.com>
2010-07-20 23:50:06 -0700

    linking into project and workstation config

---
+++ cookbooks/pivotal_workstation/recipes/linked_project_recipe.rb
@@ -0,0 +1,3 @@
+execute "This recipe is a placeholder that can be overriden in your project cookbook" do
+  command "echo 'this command intentionally left blank'"
+end
---
+++ cookbooks/pivotal_workstation/recipes/linked_workstation_recipe.rb
@@ -0,0 +1,3 @@
+execute "This recipe is a placeholder that can be overriden in your workstation cookbook" do
+  command "echo 'this command intentionally left blank'"
+end
