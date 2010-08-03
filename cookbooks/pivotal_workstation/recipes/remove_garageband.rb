f22646cbb1e37296750a8032feff345e14a810fc
ci <ci@pivotals-Mac-mini-2.local>
2010-08-02 18:50:01 -0700

    we don't need it, you might not either

---
+++ cookbooks/pivotal_workstation/recipes/remove_garageband.rb
@@ -0,0 +1,13 @@
+# This is obviously a destructive recipe.
+
+# UNTESTED USE AT YOUR OWN RISK
+
+run_unless_marker_file_exists("removing garage band") do
+  execute "Deleting Garage Band Support Files" do
+    command "rm -rf /Library/Application\ Support/GarageBand/"
+  end
+  
+  execute "Deleting Garage Band Application" do
+    command "rm -rf /Applications/GarageBand.app"
+  end
+end
