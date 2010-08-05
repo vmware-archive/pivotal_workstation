991aa8c5359e1810be6a0ff3e9146d11d683c4e0
ci <ci@pivotals-Mac-mini-2.local>
2010-08-04 17:55:57 -0700

    adding rvmrc, upgrading homebrew

--- cookbooks/pivotal_workstation/attributes/versions.rb
+++ cookbooks/pivotal_workstation/attributes/versions.rb
@@ -1,5 +1,5 @@
 node.default[:versions]= {
-  "homebrew" => "a22bf178572db574e7b3",
+  "homebrew" => "bec4d704151c7eaf3427",
   "rvm" => "9b6d75d02b583188546f",
   "Pivotal-Preferences-RubyMine" => "d0eb393ab3d4c92ac80ce3135aa9f71e59cccbd3",
   "vim" => "7.2"
