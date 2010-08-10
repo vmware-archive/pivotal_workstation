e927fb64c8d29eb960ff43a3785023729285718f
ci <ci@dyn-172-17-3-51.flood.pivotallabs.com>
2010-08-09 18:15:00 -0700

    fixing a race condition with starting mysql and setting the password.  hopefully

--- cookbooks/pivotal_workstation/recipes/mysql.rb
+++ cookbooks/pivotal_workstation/recipes/mysql.rb
@@ -3,31 +3,31 @@
 DEFAULT_PIVOTAL_MYSQL_PASSWORD = "password"
 
 include_recipe "pivotal_workstation::homebrew"
-brew_install "mysql"
 
-directory "/Users/#{WS_USER}/Library/LaunchAgents" do
-  owner WS_USER
-  action :create
+run_unless_marker_file_exists("mysql_install") do
+  brew_install "mysql"
+
+  directory "/Users/#{WS_USER}/Library/LaunchAgents" do
+    owner WS_USER
+    action :create
+  end
+
+  execute "copy mysql plist to ~/Library/LaunchAgents" do
+    command "cp /usr/local/Cellar/mysql/5.1.49/com.mysql.mysqld.plist ~/Library/LaunchAgents/"
+    user WS_USER
+  end
+
+  execute "mysql_install_db" do
+    command "mysql_install_db"
+    user WS_USER
+  end
+
+  execute "load the mysql plist into the mac daemon startup thing" do
+    command "launchctl load -w ~/Library/LaunchAgents/com.mysql.mysqld.plist; sleep 5"
+    user WS_USER
+  end
+
+  execute "set the root password to the default" do
+    command "mysqladmin -uroot password #{DEFAULT_PIVOTAL_MYSQL_PASSWORD}"
+  end
 end
-
-execute "copy mysql plist to ~/Library/LaunchAgents" do
-  command "cp /usr/local/Cellar/mysql/5.1.49/com.mysql.mysqld.plist ~/Library/LaunchAgents/"
-  user WS_USER
-end
-
-execute "mysql_install_db" do
-  command "mysql_install_db"
-  user WS_USER
-end
-
-execute "load the mysql plist into the mac daemon startup thing" do
-  command "launchctl load -w ~/Library/LaunchAgents/com.mysql.mysqld.plist"
-  user WS_USER
-end
-
-execute "set the root password if it is blank" do
-  command "mysqladmin -uroot password #{DEFAULT_PIVOTAL_MYSQL_PASSWORD}"
-  only_if "mysql -uroot -e 'show databases'"
-end
-
-#set the mysql password to be password, etc
