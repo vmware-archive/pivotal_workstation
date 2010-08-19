bcc423fcf4c970a2335862e2ca4a7d1b7646caf7
Matthew Kocher <ci@cis-macbook-pro.(none)>
2010-08-18 22:50:16 -0700

    Upgrading homebrew

--- cookbooks/pivotal_workstation/attributes/versions.rb
+++ cookbooks/pivotal_workstation/attributes/versions.rb
@@ -1,5 +1,5 @@
 node.default[:versions]= {
-  "homebrew" => "bec4d704151c7eaf3427",
+  "homebrew" => "ce8d2f5bce47d0a7383d",
   "rvm" => "9b6d75d02b583188546f",
   "Pivotal-Preferences-RubyMine" => "d0eb393ab3d4c92ac80ce3135aa9f71e59cccbd3",
   "vim" => "7.2"
