432315f7b94eb4767a0484dcbf59c954c3825b96
pivotal <pivotal@orange.flood.pivotallabs.com>
2010-09-21 12:45:11 -0700

    adding a recipe to disable front row

---
+++ cookbooks/pivotal_workstation/recipes/disable_front_row.rb
@@ -0,0 +1,4 @@
+execute "move front row out of the way" do
+  command "mv /System/Library/CoreServices/Front\\ Row.app /System/Library/CoreServices/Front_Row_disabled.app"
+  not_if { File.exists?("/System/Library/CoreServices/Front_Row_disabled.app") }
+end
