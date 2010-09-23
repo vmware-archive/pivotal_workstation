692de7a0058acbc8766e2cbd255e4b2d0ce465ba
pivotal <pivotal@orange.flood.pivotallabs.com>
2010-09-23 15:00:46 -0700

    Upgrading homebrew

--- cookbooks/pivotal_workstation/attributes/versions.rb
+++ cookbooks/pivotal_workstation/attributes/versions.rb
@@ -1,5 +1,5 @@
 node.default[:versions]= {
-  "homebrew" => "ce8d2f5bce47d0a7383d",
+  "homebrew" => "a37f156011e5b00a1170",
   "rvm" => "9b6d75d02b583188546f",
   "Pivotal-Preferences-RubyMine" => "d0eb393ab3d4c92ac80ce3135aa9f71e59cccbd3",
   "vim" => "7.2"
