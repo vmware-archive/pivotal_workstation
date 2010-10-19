e96316afe238f7b6b461019d132f4a563b9046ab
Kevin Fitzpatrick & Matthew DuVall <pair+kevin+mduvall@pivotallabs.com>
2010-10-19 15:52:18 -0700

    Updating nginx config

--- cookbooks/pivotal_workstation/recipes/nginx.rb
+++ cookbooks/pivotal_workstation/recipes/nginx.rb
@@ -3,11 +3,7 @@ include_recipe "pivotal_workstation::ssl_certificate"
 
 run_unless_marker_file_exists("nginx_7_67") do
   brew_install "nginx"
-  
-  template "/usr/local/etc/nginx/nginx.conf" do
-    source "nginx.conf.erb"
-    owner WS_USER
-  end
+
   
   execute "stop nginx, don't worry if it can't" do
     command "/usr/local/sbin/nginx -s stop || true"
@@ -23,3 +19,8 @@ run_unless_marker_file_exists("nginx_7_67") do
 
 end
 
+  
+template "/usr/local/etc/nginx/nginx.conf" do
+  source "nginx.conf.erb"
+  owner WS_USER
+end
