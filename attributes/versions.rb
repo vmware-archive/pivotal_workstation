cbecd8ca776be7c07b6b06fa81d6982a19b91ad8
Engineer <engineer@dyn-172-17-3-89.flood.pivotallabs.com>
2010-11-24 17:09:25 -0800

    upgrading rvm version

--- attributes/versions.rb
+++ attributes/versions.rb
@@ -1,6 +1,6 @@
 node.default[:versions]= {
   "homebrew" => "a37f156011e5b00a1170",
-  "rvm" => "9b6d75d02b583188546f",
+  "rvm" => "4370a089470a9587807e",
   "Pivotal-Preferences-RubyMine" => "d0eb393ab3d4c92ac80ce3135aa9f71e59cccbd3",
   "vim" => "7.2"
 }
