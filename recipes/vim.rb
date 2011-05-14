c11ef386d79e4123307f4b1587b5548d047299d8
Michael Sofaer <msofaer@pivotallabs.com>
2011-05-13 18:20:19 -0700

    Switch to the pivotal/vim-config repo for configuring vim

--- recipes/vim.rb
+++ recipes/vim.rb
@@ -1,16 +1,32 @@
 ::VIM_HOME = "#{WS_HOME}/.vim"
+include_recipe "pivotal_workstation::homebrew"
 include_recipe "pivotal_workstation::ack"
+include_recipe "pivotal_workstation::git"
 
 brew_install "macvim"
 
-remote_directory VIM_HOME do
-  source "vim"
-  owner WS_USER
+execute "clone the .vim directory" do
+  user WS_USER
+  command "git clone https://github.com/pivotal/vim-config.git #{VIM_HOME}"
+  not_if "cd #{VIM_HOME} && git remote -v | grep pivotal/vim-config"
+end
+
+execute "ensure we're on the approved hash" do
+  user WS_USER
+  command "cd #{VIM_HOME} && git checkout 960967514858e889dfc1"
+end
+
+execute "get the submodules" do
+  user WS_USER
+  command "cd #{VIM_HOME} && git submodule update --init"
 end
 
 link "#{WS_HOME}/.vimrc" do
   to "#{VIM_HOME}/vimrc"
+  owner WS_USER
 end
+
+brew_install "ctags"
 
 execute "compile command-t" do
   cwd "#{VIM_HOME}/bundle/command-t/ruby/command-t"
