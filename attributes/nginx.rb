efa81dff298cd688e6e92c1ecf10f82e2364ad1d
pivotal-sumuru <pivotal@collingwood.flood.pivotallabs.com>
2011-05-19 09:58:31 -0700

    Set nginx to use port 8080 instead of 8888 because jasmine uses 8888.

--- attributes/nginx.rb
+++ attributes/nginx.rb
@@ -1,6 +1,6 @@
 node.default["nginx_settings"]= {
   "server_name" => "localhost.local",
-  "server_port" => 8888,
+  "server_port" => 8080,
   "server_document_root"=> "/var/www/apps/example/current/public"
 }
 
