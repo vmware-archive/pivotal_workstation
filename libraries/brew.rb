86cd54f0e50614c59d77524982ec63b8e228e2fd
Kevin Fitzpatrick <pair+kevin@pivotallabs.com>
2011-06-17 14:13:03 -0700

    Add brew_update & brew_remove to the helpers

--- libraries/brew.rb
+++ libraries/brew.rb
@@ -1,10 +1,31 @@
 class Chef::Recipe
   def brew_install(package)
     include_recipe "pivotal_workstation::homebrew"
-    
+
     execute "brew install #{package}" do
       user WS_USER
       command "brew install #{package}"
+    end
+  end
+
+  def brew_remove(package)
+    include_recipe "pivotal_workstation::homebrew"
+
+    brew_installed = `brew list | grep postgresql`
+    unless brew_installed.empty?
+      execute "brew remove #{package}" do
+        user WS_USER
+        command "brew remove #{package}"
+      end
+    end
+  end
+
+  def brew_update
+    include_recipe "pivotal_workstation::homebrew"
+
+    execute "brew update" do
+      user WS_USER
+      command "brew update"
     end
   end
 end
