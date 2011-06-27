d610803d303d6d6df70414955512011df8b66807
Davis W. Frank <pair+dwfrank@pivotallabs.com>
2011-06-27 11:12:30 -0700

    updating webdriver recipe to make symlinks

--- recipes/selenium_webdriver.rb
+++ recipes/selenium_webdriver.rb
@@ -1,6 +1,6 @@
 webdriver_location = "http://selenium.googlecode.com/files/chromedriver_mac_13.0.775.0.zip"
 
-run_unless_marker_file_exists("selenium_webdriver_13_0_775") do
+run_unless_marker_file_exists("selenium_webdriver_13_0_775_3") do
   execute "download webdriver" do
     command "curl -o /tmp/webdriver.zip #{webdriver_location}"
     user WS_USER
@@ -9,4 +9,16 @@ run_unless_marker_file_exists("selenium_webdriver_13_0_775") do
   execute "unzip" do
     command "unzip -o /tmp/webdriver.zip chromedriver -d /usr/bin/"
   end
+end
+
+file "/usr/bin/chromedriver" do
+  mode "0755"
+end
+
+execute "create chromium.app symlink" do
+  command 'rm  -f "/Applications/Chromium.app" && ln -s "/Applications/Google Chrome.app" "/Applications/Chromium.app"'
+end
+
+execute "create contents symlink" do
+  command 'rm -f "/Applications/Chromium.app/Contents/MacOS/Chromium" && ln -s "/Applications/Chromium.app/Contents/MacOS/Google Chrome" "/Applications/Chromium.app/Contents/MacOS/Chromium"'
 end
