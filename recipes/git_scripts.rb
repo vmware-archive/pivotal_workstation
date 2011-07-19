e9f235582aedcbdcf47833da0392e5f98b181154
Brian Cunnie & Kevin Fitzpatrick <pair+cunnie+kevin@pivotallabs.com>
2011-07-19 11:37:51 -0700

    /usr/local/bin is a dependency

--- recipes/git_scripts.rb
+++ recipes/git_scripts.rb
@@ -1,3 +1,5 @@
+include_recipe "pivotal_workstation::user_owns_usr_local"
+
 run_unless_marker_file_exists("git_scripts") do
   execute "put git-scripts in /usr/local/bin" do
     command "cd /usr/local/bin && curl -L http://github.com/pivotal/git_scripts/tarball/master | gunzip | tar xvf - --strip=2"
