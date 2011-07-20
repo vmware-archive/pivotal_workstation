2d85d8000fe9119578d76bbf1b015af1b4f5633b
Brian Cunnie & Kevin Fitzpatrick <pair+cunnie+kevin@pivotallabs.com>
2011-07-20 10:05:14 -0700

    removed extraneous comment

--- recipes/enable_screen_sharing.rb
+++ recipes/enable_screen_sharing.rb
@@ -1,6 +1,5 @@
 execute "Enabling Screen Sharing for All Users" do
   command "/System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -activate -configure -access -off -restart -agent -privs -all"
-  #command "/System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -deactivate -stop"  # to test the test
 end
 
 ruby_block "test to see if screen-sharing is enabled" do
