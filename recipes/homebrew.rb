0345a1f408340f30ed44208791ac2958135c700c
Brian Cunnie & Kevin Fitzpatrick <pair+cunnie+kevin@pivotallabs.com>
2011-07-19 17:54:58 -0700

    re-wrote homebrew; no marker_file, but now a test

--- recipes/homebrew.rb
+++ recipes/homebrew.rb
@@ -1,26 +1,19 @@
+include_recipe "pivotal_workstation::user_owns_usr_local"
+
 homebrew_git_revision_hash  = version_string_for("homebrew")
 
-run_unless_marker_file_exists(marker_version_string_for("homebrew")) do
-
-  directory "/usr/local" do
-    owner WS_USER
-  end
-
-  execute "your current user owns /usr/local" do
-    command "chown -R #{WS_USER} /usr/local"
-  end
-
-  execute "install homebrew" do
-    command "curl -Lsf http://github.com/mxcl/homebrew/tarball/#{homebrew_git_revision_hash} | tar xvz -C/usr/local --strip 1"
-    user WS_USER
-  end
-
-  ruby_block "check homebrew" do
-    block do
-      result = `sudo -u #{WS_USER} brew --version`
-      raise("brew not working: #{result}") unless result.strip.to_f >= 0.6
-    end
-  end
-
+git "/usr/local" do
+  repository "https://github.com/mxcl/homebrew.git"
+  revision homebrew_git_revision_hash
+  destination "/usr/local"
+  action :sync
 end
 
+ruby_block "Check that homebrew is running & working" do
+  block do
+    `brew --version`
+    if $? != 0
+      raise "Couldn't find brew"
+    end
+  end
+end
