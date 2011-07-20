1e07e6a8f8a0d53de8d174a2e9dee05b95124bc2
Brian Cunnie & Kevin Fitzpatrick <pair+cunnie+kevin@pivotallabs.com>
2011-07-20 10:24:55 -0700

    base recipe changes gem installs so no rdoc nor ri

--- recipes/base_lion_development.rb
+++ recipes/base_lion_development.rb
@@ -4,4 +4,5 @@ include_recipe "pivotal_workstation::gitx"
 include_recipe "pivotal_workstation::turn_on_ssh"
 include_recipe "pivotal_workstation::enable_screen_sharing"
 include_recipe "pivotal_workstation::postgres"
+include_recipe "pivotal_workstation::gem_no_rdoc_no_ri"
 #include_recipe "pivotal_workstation::mysql"  # broken
