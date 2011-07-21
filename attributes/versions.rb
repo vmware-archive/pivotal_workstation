769ab619b02d25ddc677202ae8d7c92f637adbcb
Brian Cunnie & Kevin Fitzpatrick <pair+cunnie+kevin@pivotallabs.com>
2011-07-21 12:29:16 -0700

    trying a fresher version of rvm

--- attributes/versions.rb
+++ attributes/versions.rb
@@ -1,6 +1,6 @@
 node.default[:versions]= {
   "homebrew" => "6d15547fa6d422fd55e3c7c7dcf617176afe55da",
-  "rvm" => "4370a089470a9587807e",
+  "rvm" => "77ebecce67e30bd0f8d0",
   "Pivotal-Preferences-RubyMine" => "d0eb393ab3d4c92ac80ce3135aa9f71e59cccbd3",
   "RubyMine-VersionString" => "30",
   "vim" => "7.2"
