0173304b23cff41dde7abb97f74d4578864688c8
Pivotal Labs <pivotal@market.flood.pivotallabs.com>
2011-07-22 11:57:46 -0700

    Don't install home brew twice. Fixed issue with non-empty /usr/local

--- recipes/homebrew.rb
+++ recipes/homebrew.rb
@@ -2,11 +2,19 @@ include_recipe "pivotal_workstation::user_owns_usr_local"
 
 homebrew_git_revision_hash  = version_string_for("homebrew")
 
-git "/usr/local" do
-  repository "https://github.com/mxcl/homebrew.git"
-  revision homebrew_git_revision_hash
-  destination "/usr/local"
-  action :sync
+if (`which brew`.empty?)
+  git "/tmp/homebrew" do
+    repository "https://github.com/mxcl/homebrew.git"
+    revision homebrew_git_revision_hash
+    destination "/tmp/homebrew"
+    action :sync
+    user WS_USER
+  end
+
+  execute "Copying homebrew's .git to /usr/local" do
+    command "rsync -axSH /tmp/homebrew/ /usr/local/"
+    user WS_USER
+  end
 end
 
 ruby_block "Check that homebrew is running & working" do
