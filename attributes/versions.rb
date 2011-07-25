241b44ef7dfd73063eb0417a914963c35c4eb80b
Pivotal Labs <pivotal@market.flood.pivotallabs.com>
2011-07-25 14:34:50 -0700

    newer homebrew to fix mysql error (fail)

--- attributes/versions.rb
+++ attributes/versions.rb
@@ -1,5 +1,5 @@
 node.default[:versions]= {
-  "homebrew" => "6d15547fa6d422fd55e3c7c7dcf617176afe55da",
+  "homebrew" => "3585b00cf396c2c5e1d0",
   "rvm" => "52b9bedf4ddc769d5332",
   "Pivotal-Preferences-RubyMine" => "d0eb393ab3d4c92ac80ce3135aa9f71e59cccbd3",
   "RubyMine-VersionString" => "32",
