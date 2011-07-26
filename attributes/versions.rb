e81ac86997c490e6d849b4be7fb777303711d23d
Brian Cunnie <cunnie@pivotallabs.com>
2011-07-25 18:25:21 -0700

    oops, forgot a ,

--- attributes/versions.rb
+++ attributes/versions.rb
@@ -1,5 +1,5 @@
 node.default[:versions]= {
-  "homebrew" => "ee2f510e18c1020ae5583a89cefe8e160f76c886"
+  "homebrew" => "ee2f510e18c1020ae5583a89cefe8e160f76c886",
   "rvm" => "52b9bedf4ddc769d5332",
   "Pivotal-Preferences-RubyMine" => "d0eb393ab3d4c92ac80ce3135aa9f71e59cccbd3",
   "RubyMine-VersionString" => "32",
