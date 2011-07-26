638a82368391b5f4b6c546851f1bbd6badaa6916
Pivotal Labs <pivotal@market.flood.pivotallabs.com>
2011-07-25 17:06:57 -0700

    VMWare Fusion recipe.

---
+++ attributes/fusion.rb
@@ -0,0 +1,1 @@
+node.default["fusion_download_uri"]="https://download2.vmware.com/software/fusion/VMware-Fusion-3.1.3-416484-light.dmg?HashKey=45483b3da7f639b5a5c2c8efb8881291&AuthKey=1311638507_d9d6f132cc139f5e8eeb02d4c90cdec1"
