bae4ab46595398e088a4b5ee1b6813b46feff2fc
Pivotal Labs <pivotal@market.flood.pivotallabs.com>
2011-07-25 17:03:54 -0700

    Fixed pathname for unmount; fixed error messages that said "chrome" instead of java"

--- recipes/java.rb
+++ recipes/java.rb
@@ -1,6 +1,5 @@
 `pkgutil --pkgs=com.apple.pkg.JavaForMacOSX107`
 if $? != 0
-  #raise "Java not installed!"
 
   remote_file "/tmp/java.dmg" do
     source node["java_download_uri"]
@@ -17,11 +16,11 @@ if $? != 0
   end
 
   execute "unmount dmg" do
-    command "hdiutil detach /Volumes/Google\\ Chrome"
+    command "hdiutil detach /Volumes/Java\\ for\\ Mac\\ OS\\ X\\ 10.7"
     user WS_USER
   end
 
-  ruby_block "test to see if Chrome was installed" do
+  ruby_block "test to see if java was installed" do
     block do
       `pkgutil --pkgs=com.apple.pkg.JavaForMacOSX107`
       raise "Java install failed" if $? != 0
