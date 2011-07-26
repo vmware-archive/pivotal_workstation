774dbc5e862b7c9827f0d4decfbe54c323517562
Pivotal Labs <pivotal@market.flood.pivotallabs.com>
2011-07-25 17:51:05 -0700

    broken homebrew version

--- attributes/versions.rb
+++ attributes/versions.rb
@@ -1,5 +1,5 @@
 node.default[:versions]= {
-  "homebrew" => "3585b00cf396c2c5e1d0",
+  "homebrew" => "ee2f510e18c1020ae558",
   "rvm" => "52b9bedf4ddc769d5332",
   "Pivotal-Preferences-RubyMine" => "d0eb393ab3d4c92ac80ce3135aa9f71e59cccbd3",
   "RubyMine-VersionString" => "32",
