25330de937cb6e92404cbbdf8d286e43cdeb50f5
Brian Cunnie <cunnie@pivotallabs.com>
2011-07-27 11:01:33 -0700

    brought in the latest versions

--- attributes/versions.rb
+++ attributes/versions.rb
@@ -1,6 +1,6 @@
 node.default[:versions]= {
-  "homebrew" => "ee2f510e18c1020ae5583a89cefe8e160f76c886",
-  "rvm" => "52b9bedf4ddc769d5332",
+  "homebrew" => "f895bee94ed8380970d627d6fcb2db49c38c5f48",
+  "rvm" => "019d77a93662a4a6c56e46416776e92ca9a42d58",
   "Pivotal-Preferences-RubyMine" => "d0eb393ab3d4c92ac80ce3135aa9f71e59cccbd3",
   "RubyMine-VersionString" => "32",
   "vim" => "7.2"
