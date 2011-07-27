2791b1ca50dea79f5f6cb1f49b016aab53158bbe
Brian Cunnie <cunnie@pivotallabs.com>
2011-07-27 11:01:14 -0700

    updated fusion URI

--- attributes/fusion.rb
+++ attributes/fusion.rb
@@ -1,1 +1,1 @@
-node.default["fusion_download_uri"]="https://download2.vmware.com/software/fusion/VMware-Fusion-3.1.3-416484-light.dmg?HashKey=45483b3da7f639b5a5c2c8efb8881291&AuthKey=1311638507_d9d6f132cc139f5e8eeb02d4c90cdec1"
+node.default["fusion_download_uri"]="https://download2.vmware.com/software/fusion/VMware-Fusion-3.1.3-416484-light.dmg?HashKey=f381863921400dbf00aafbceed3ba430&AuthKey=1311786532_dd802694bf5ecb329595a9154a4ba945"
