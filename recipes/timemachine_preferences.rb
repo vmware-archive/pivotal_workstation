59104f26d04849d8feef56fa818c72c23597c9b0
Brian Cunnie & Matthew Kocher <pair+cunnie+mkocher@pivotallabs.com>
2011-08-01 16:33:41 -0700

    fixed problems running as root user

--- recipes/timemachine_preferences.rb
+++ recipes/timemachine_preferences.rb
@@ -6,17 +6,17 @@ end
 ruby_block "TimeMachine should NOT appear in the status bar" do
   block do 
     new_menuExtras=""
-    `defaults read com.apple.systemuiserver menuExtras`.split("\n").each do |line|
+    `sudo -u #{WS_USER} defaults read com.apple.systemuiserver menuExtras`.split("\n").each do |line|
       if line !~ /TimeMachine.menu/
 	new_menuExtras = new_menuExtras + line + "\n"
       end
     end
-    `defaults write com.apple.systemuiserver menuExtras \'#{new_menuExtras}\'`
+    `sudo -u #{WS_USER} defaults write com.apple.systemuiserver menuExtras \'#{new_menuExtras}\'`
     `killall -HUP SystemUIServer`
   end
 end
 
 execute "make sure it doesn't exist"   do
-  command `! defaults read com.apple.systemuiserver menuExtras | grep TimeMachine.menu`
+  command "! defaults read com.apple.systemuiserver menuExtras | grep TimeMachine.menu"
   user WS_USER
 end
