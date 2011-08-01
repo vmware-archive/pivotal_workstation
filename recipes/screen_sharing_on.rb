b76b423311d75df2db2fe90bdc75ad039de107c9
Brian Cunnie & Matthew Kocher <pair+cunnie+mkocher@pivotallabs.com>
2011-08-01 16:58:29 -0700

    needed to give ALL users privileges

--- recipes/screen_sharing_on.rb
+++ recipes/screen_sharing_on.rb
@@ -1,5 +1,5 @@
 execute "Enabling Screen Sharing for All Users" do
-  command "/System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -activate -configure -access -off -restart -agent -privs -all"
+  command "/System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -activate -configure -access -off -restart -agent -privs -all -allowAccessFor -allUsers"
 end
 
 ruby_block "test to see if screen-sharing is enabled" do
