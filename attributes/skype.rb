e98bbe6ae89178008f0f7e769bc750cd9d87ff1f
Brian Cunnie & Matthew Kocher <pair+cunnie+mkocher@pivotallabs.com>
2011-08-03 10:15:38 -0700

    bumped version of skype

--- attributes/skype.rb
+++ attributes/skype.rb
@@ -1,1 +1,1 @@
-node.default["skype_download_uri"]="http://download.skype.com/macosx/Skype_5.2.0.1523.dmg"
+node.default["skype_download_uri"]="http://download.skype.com/macosx/Skype_5.2.0.1572.dmg"
