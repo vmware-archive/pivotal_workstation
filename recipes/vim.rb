5934237ba98c4d65b9506a3e7dc7a5d1899678c5
Brian Cunnie & Matthew Kocher <pair+cunnie+mkocher@pivotallabs.com>
2011-08-03 15:21:58 -0700

    'vi' now behaves as a sane 'vim'

--- recipes/vim.rb
+++ recipes/vim.rb
@@ -5,6 +5,14 @@ include_recipe "pivotal_workstation::git"
 
 brew_install "macvim"
 
+execute "move existing vim_home out of the way if necessary" do
+  user WS_USER
+  command "mv #{VIM_HOME} #{WS_HOME}/.vim-old"
+  only_if {
+    File.exists?(VIM_HOME) && !system("cd #{VIM_HOME} && git remote -v | grep pivotal/vim-config")
+  }
+end
+
 execute "clone the .vim directory" do
   user WS_USER
   command "git clone https://github.com/pivotal/vim-config.git #{VIM_HOME}"
@@ -13,7 +21,7 @@ end
 
 execute "ensure we're on the approved hash" do
   user WS_USER
-  command "cd #{VIM_HOME} && git checkout a3d2e49c04cb9258b195"
+  command "cd #{VIM_HOME} && git checkout 87392f4f200f005245dca5cf6a7a063f8e68b9df"
 end
 
 execute "get the submodules" do
@@ -33,3 +41,5 @@ execute "compile command-t" do
   command "ruby extconf.rb && make"
   user WS_USER
 end
+
+bash_profile_include("vi_is_minimal_vim")
