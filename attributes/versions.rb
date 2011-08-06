383f05d8535aafec51b1c798f99ceafb6a156cbb
Brian Cunnie & Matthew Kocher <pair+brian+mkocher@pivotallabs.com>
2011-08-05 18:43:32 -0700

    left out a comma

--- attributes/versions.rb
+++ attributes/versions.rb
@@ -1,5 +1,5 @@
 node.default[:versions]= {
-  "homebrew" => "2abfba1a91bfda3017d23a631ef237933e29c2f5"
+  "homebrew" => "2abfba1a91bfda3017d23a631ef237933e29c2f5",
   "rvm" => "019d77a93662a4a6c56e46416776e92ca9a42d58",
   "Pivotal-Preferences-RubyMine" => "d0eb393ab3d4c92ac80ce3135aa9f71e59cccbd3",
   "RubyMine-VersionString" => "32",
