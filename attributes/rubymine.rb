7dff3ce282fe9ae29b83f95b91fe790a0dead8c3
Brian Cunnie & Matthew Kocher <pair+brian+mkocher@pivotallabs.com>
2011-08-05 17:43:26 -0700

    bumped RubyMine to 3.2.3

--- attributes/rubymine.rb
+++ attributes/rubymine.rb
@@ -1,4 +1,4 @@
-node.default["rubymine_version"]="3.2.2"
-node.default["rubymine_download_uri"]="http://download.jetbrains.com/ruby/RubyMine#{node.default["rubymine_version"]}.dmg"
+node.default["rubymine_version"]="3.2.3"
+node.default["rubymine_download_uri"]="http://download.jetbrains.com/ruby/RubyMine-#{node.default["rubymine_version"]}.dmg"
 node.default["rubymine_dmg_mnt"]="/Volumes/RubyMine #{node.default["rubymine_version"]}"
 node.default["rubymine_basename"]="RubyMine #{node.default["rubymine_version"]}.app"
