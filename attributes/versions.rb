1d00b9985795a782a446dd0e24751088e7df08c0
Brian Cunnie <brian.cunnie@gmail.com>
2011-08-09 22:47:48 -0700

    bumped homebrew & rvm revisions

--- attributes/versions.rb
+++ attributes/versions.rb
@@ -1,6 +1,6 @@
 node.default[:versions]= {
-  "homebrew" => "2abfba1a91bfda3017d23a631ef237933e29c2f5",
-  "rvm" => "019d77a93662a4a6c56e46416776e92ca9a42d58",
+  "homebrew" => "81f8f912bb9ed5784c33b5dec406db862e74d5ee",
+  "rvm" => "d5872bce8aa453f688cd942f4a1698105e870656",
   "Pivotal-Preferences-RubyMine" => "d0eb393ab3d4c92ac80ce3135aa9f71e59cccbd3",
   "RubyMine-VersionString" => "32",
   "vim" => "7.2"
