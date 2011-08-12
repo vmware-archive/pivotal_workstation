bdb5db8a580eafaa7bc298ca9382cbce958adfb2
Brian Cunnie & Matthew Kocher <pair+brian+mkocher@pivotallabs.com>
2011-08-11 17:26:48 -0700

    added backgrounds & icons

--- recipes/pivotal_logos.rb
+++ recipes/pivotal_logos.rb
@@ -1,4 +1,4 @@
-%w{Icons Backgrounds}.each do |dir|
+%w{Icons BackgroundsPrimary BackgroundsSecondary}.each do |dir|
   directory "#{WS_HOME}/Pictures/#{dir}" do
     mode "0755"
     owner WS_USER
@@ -8,14 +8,14 @@ end
 
 %w{blue green orange violet}.each do |color|
   filename="pivID_#{color}-1004x400.png"
-  remote_file "#{WS_HOME}/Pictures/Backgrounds/#{filename}" do
+  remote_file "#{WS_HOME}/Pictures/BackgroundsPrimary/#{filename}" do
     source "http://cheffiles.pivotallabs.com/#{filename}"
     :create_if_missing
   end
 end
 
 %w{BackToTheEdward.png}.each do |filename|
-  remote_file "#{WS_HOME}/Pictures/Backgrounds/#{filename}" do
+  remote_file "#{WS_HOME}/Pictures/BackgroundsSecondary/#{filename}" do
     source "http://cheffiles.pivotallabs.com/#{filename}"
     :create_if_missing
   end
@@ -28,3 +28,51 @@ end
     :create_if_missing
   end
 end
+
+ruby_block "install the pivotal tracker picture" do
+  block do
+    `dscl . delete /Users/#{WS_USER} JPEGPhoto`
+    `dscl . create /Users/#{WS_USER} Picture "#{WS_HOME}/Pictures/Icons/Pivotal Tracker.png"`
+  end
+end
+
+gem_package("plist")
+
+ruby_block "install the pivotal backgrounds" do
+  block do
+    `dscl . delete /Users/#{WS_USER} JPEGPhoto`
+    `dscl . create /Users/#{WS_USER} Picture "#{WS_HOME}/Pictures/Icons/Pivotal Tracker.png"`
+
+    Gem.clear_paths
+    require 'rubygems'
+
+    require 'plist'
+    # FIXME:  if plist  doesn't exist, create it.
+    plist_file = "#{ENV['HOME']}/Library/Preferences/com.apple.desktop.plist"
+    `plutil -convert xml1 #{plist_file}`
+    desktop_plist = Plist::parse_xml(plist_file)
+    desktop_plist["Background"] =
+      { "default" =>
+        { "BackgroundColor" =
+            [ 0.0, 0.250980406999588, 0.501960813999176 ],
+          "Change" => "TimeInterval",
+          "ChangePath" => "/Users/pivotal/Pictures/BackgroundsPrimary",
+          "ChangeTime" => 5.0,
+          "DSKDesktopPrefPane" => {
+            "UserFolderPaths" = [
+              "/Users/pivotal/Pictures/BackgroundsPrimary",
+              "/Users/pivotal/Pictures/BackgroundsSecondary"
+            ]
+          }
+          "DrawBackgroundColor" => true,
+          "LastName" => "pivID_orange-1004x400.png",
+          "NewChangePath" => "~/Pictures/BackgroundsPrimary",
+          "NoImage" => false,
+          "Placement" => "Centered",
+          "Random" => false,
+        },
+      }
+  end
+end
+
+
