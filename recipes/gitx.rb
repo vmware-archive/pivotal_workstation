be42bd52df530eacbee060190e14f60efe110ff2
Brian Cunnie & Matthew Kocher <pair+cunnie+mkocher@pivotallabs.com>
2011-08-15 09:53:02 -0700

    added symbolic link for terminal access

--- recipes/gitx.rb
+++ recipes/gitx.rb
@@ -1,4 +1,6 @@
 GITX_PATH = "/Applications/GitX.app"
+GITX_LINK = "/usr/local/bin/gitx"
+include_recipe "pivotal_workstation::user_owns_usr_local"
 
 unless File.exists?(GITX_PATH)
 
@@ -17,9 +19,22 @@ unless File.exists?(GITX_PATH)
     user WS_USER
   end
 
-  ruby_block "test to see if GitX was installed" do
+  ruby_block "test to see if GitX.app was installed" do
     block do
-      raise "GitX was not installed" unless File.exists?(GITX_PATH)
+      raise "GitX.app was not installed" unless File.exists?(GITX_PATH)
+    end
+  end
+end
+
+unless File.exists?(GITX_LINK)
+  # ln -s /Applications/GitX.app/Contents/Resources/gitx /usr/local/bin/gitx
+  link "/usr/local/bin/gitx" do
+    to "/Applications/GitX.app/Contents/Resources/gitx"
+  end
+
+  ruby_block "test to see if gitx link was installed" do
+    block do
+      raise "gitx link was not installed" unless File.exists?(GITX_PATH)
     end
   end
 end
