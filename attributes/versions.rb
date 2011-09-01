11170cd531f176c84ea20808a8595a702a017008
Brian Cunnie & Matthew Kocher <pair+cunnie+mkocher@pivotallabs.com>
2011-09-01 14:48:25 -0700

    bumped up the revisions for brew & rvm

--- attributes/versions.rb
+++ attributes/versions.rb
@@ -1,6 +1,6 @@
 node.default[:versions]= {
-  "homebrew" => "97b62b8b3f1c8973cf8a2575bfc98706ccbbde39",
-  "rvm" => "cd2786c309faaf92efec02d6c7b11c7dae8d27e4",
+  "homebrew" => "9b4f9570991160b38c992758e13dd2dfdbdbb0f9"
+  "rvm" => "c2a58c6779d19be17a3c3cfddc13e6a0297648b8"
   "Pivotal-Preferences-RubyMine" => "d0eb393ab3d4c92ac80ce3135aa9f71e59cccbd3",
   "RubyMine-VersionString" => "32",
 }
