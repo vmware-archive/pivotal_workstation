cc700214947e435fb900f01b48845c6020475e7b
Brian Cunnie & Matthew Kocher <pair+cunnie+mkocher@pivotallabs.com>
2011-09-01 14:55:42 -0700

    Oops--forgot the commas.

--- attributes/versions.rb
+++ attributes/versions.rb
@@ -1,6 +1,6 @@
 node.default[:versions]= {
-  "homebrew" => "9b4f9570991160b38c992758e13dd2dfdbdbb0f9"
-  "rvm" => "c2a58c6779d19be17a3c3cfddc13e6a0297648b8"
+  "homebrew" => "9b4f9570991160b38c992758e13dd2dfdbdbb0f9",
+  "rvm" => "c2a58c6779d19be17a3c3cfddc13e6a0297648b8",
   "Pivotal-Preferences-RubyMine" => "d0eb393ab3d4c92ac80ce3135aa9f71e59cccbd3",
   "RubyMine-VersionString" => "32",
 }
