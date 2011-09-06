bd7e19e13ee10fdefb3dad295ac8130a4b53218d
Alex Lobascio & Ian Lesperance <pair+alobascio+ilesperance@pivotallabs.com>
2011-09-06 16:43:32 -0700

    added recipe for setting up SSH keys for github

---
+++ recipes/github_ssh_keys.rb
@@ -0,0 +1,16 @@
+execute "create SSH key pair for Github" do
+  command "ssh-keygen -N '' -f #{WS_HOME}/.ssh/id_github_#{node["github_project"]}"
+  user WS_USER
+  not_if "test -e #{WS_HOME}/.ssh/id_github_#{node["github_project"]}"
+end
+
+execute "symlink Github key for git-project" do
+  command "ln -nfs #{WS_HOME}/.ssh/id_github_{#{node["github_project"]},current}"
+  user WS_USER
+end
+
+execute "add Github configuration to .ssh/config" do
+  config = "\n\nHost github.com\n  User git\n  IdentityFile #{WS_HOME}/.ssh/id_github_current"
+  command "echo '#{config}' >> #{WS_HOME}/.ssh/config"
+  not_if "grep 'id_github_current' #{WS_HOME}/.ssh/config"
+end
