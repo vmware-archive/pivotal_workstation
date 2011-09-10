ba010ace07914f309cfb228487f543f793402bfe
Brian Cunnie & Matthew Kocher <pair+cunnie+mkocher@pivotallabs.com>
2011-09-09 18:03:37 -0700

    new recipe for chrome

--- recipes/chrome.rb
+++ recipes/chrome.rb
@@ -1,28 +1,6 @@
-unless File.exists?("/Applications/Google Chrome.app")
-
-  remote_file "/tmp/chrome.dmg" do
-    source node["chrome_download_uri"]
-    mode "0644" 
-  end
-  
-  execute "mount chrome dmg" do
-    command "hdid /tmp/chrome.dmg"
-    user WS_USER
-  end
-
-  execute "copy chrome to /Applications" do
-    command "cp -rf /Volumes/Google\\ Chrome/Google\\ Chrome.app /Applications/"
-    user WS_USER
-  end
-
-  execute "unmount dmg" do
-    command "hdiutil detach /Volumes/Google\\ Chrome"
-    user WS_USER
-  end
-
-  ruby_block "test to see if Chrome was installed" do
-    block do
-      raise "Chrome install failed" unless File.exists?("/Applications/Google Chrome.app")
-    end
-  end
+pivotal_workstation_package "Google Chrome" do
+  dmg_name "googlechrome"
+  source "https://dl-ssl.google.com/chrome/mac/stable/GGRM/googlechrome.dmg"
+  checksum "7daa2dc5c46d9bfb14f1d7ff4b33884325e5e63e694810adc58f14795165c91a"
+  action :install
 end
