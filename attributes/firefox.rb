836b2db58027d1169da6d18a9189c29763a06a45
Brian Cunnie & Matthew Kocher <pair+cunnie+mkocher@pivotallabs.com>
2011-09-12 14:30:50 -0700

    re-factored firefox

--- attributes/firefox.rb
+++
@@ -1,2 +0,0 @@
-node.default["firefox_download_uri"]="http://download.mozilla.org/?product=firefox-6.0&os=osx&lang=en-US"
-node.default["firefox_app_path"]="/Applications/Firefox.app"
--- recipes/firefox.rb
+++ recipes/firefox.rb
@@ -1,28 +1,5 @@
-unless File.exists?(node["firefox_app_path"])
-
-  remote_file "/tmp/firefox.dmg" do
-    source node["firefox_download_uri"]
-    mode "0644" 
-  end
-  
-  execute "mount firefox dmg" do
-    command "hdid /tmp/firefox.dmg"
-    user WS_USER
-  end
-  
-  execute "copy firefox to /Applications" do
-    command "cp -rf /Volumes/Firefox/Firefox.app #{Regexp.escape(node["firefox_app_path"])}"
-    user WS_USER
-  end
-  
-  execute "unmount dmg" do
-    command "hdiutil detach /Volumes/Firefox"
-    user WS_USER
-  end
-
-  ruby_block "test to see if Firefox was installed" do
-    block do
-      raise "Firefox install failed" unless File.exists?(node["firefox_app_path"])
-    end
-  end
+pivotal_workstation_package "Firefox" do
+  source "http://download.mozilla.org/?product=firefox-6.0.2&os=osx&lang=en-US"
+  action :install
+  checksum "6c594a3bbd642dcdbf34cdcfa67fd1ec89855f37cbd42feea7813e625335c19c"
 end
