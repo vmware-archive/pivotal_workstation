b12477508c6faad04aeb0ff71f329cfbda0f40c2
Brian Cunnie & Matthew Kocher <pair+cunnie+mkocher@pivotallabs.com>
2011-09-12 15:40:29 -0700

    re-factored dropbox

--- recipes/dropbox.rb
+++ recipes/dropbox.rb
@@ -1,30 +1,6 @@
-unless File.exists?("/Applications/Dropbox.app")
-
-  remote_file "/tmp/dropbox.dmg" do
-    source node["dropbox_download_uri"]
-    mode "0644" 
-  end
-  
-  execute "mount dropbox dmg" do
-    command "hdid /tmp/dropbox.dmg"
-    user WS_USER
-  end
-
-  execute "copy dropbox to /Applications" do
-    # use rsync instead of cp because /Dropbox.app/Contents/Resources/lib/python2.5/site.py
-    # is a broken link, and cp exits non-zero
-    command "rsync -a /Volumes/Dropbox\\ Installer/Dropbox.app /Applications/"
-    user WS_USER
-  end
-
-  execute "unmount dmg" do
-    command "hdiutil detach /Volumes/Dropbox\\ Installer"
-    user WS_USER
-  end
-
-  ruby_block "test to see if Dropbox was installed" do
-    block do
-      raise "Dropbox install failed" unless File.exists?("/Applications/Dropbox.app")
-    end
-  end
+pivotal_workstation_package "Dropbox" do
+  volumes_dir "Dropbox Installer"
+  source "http://www.dropbox.com/download?plat=mac"
+  checksum "a721c6e7fa9ae0d07f220901764ac65190d9299c7a0b62cd8a6822a27365e275"
+  action :install
 end
