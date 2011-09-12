710ff8f0c6f703f64d461390e554665a205d792e
Brian Cunnie & Matthew Kocher <pair+cunnie+mkocher@pivotallabs.com>
2011-09-12 14:59:04 -0700

    re-factored for brevity

--- attributes/libreoffice.rb
+++
@@ -1,3 +0,0 @@
-node.default["libreoffice_download_uri"]="http://download.documentfoundation.org/libreoffice/stable/3.3.4/mac/x86/LibO_3.3.4_MacOS_x86_install_en-US.dmg"
-node.default["libreoffice_dmg_mnt"]="/Volumes/LibreOffice 3.3"
-node.default["libreoffice_basename"]="LibreOffice.app"
--- recipes/libreoffice.rb
+++ recipes/libreoffice.rb
@@ -1,28 +1,6 @@
-unless File.exists?("/Applications/" + node["libreoffice_basename"])
-
-  remote_file "/tmp/libreoffice.dmg" do
-    source node["libreoffice_download_uri"]
-    mode "0644" 
-  end
-  
-  execute "mount libreoffice dmg" do
-    command "hdid /tmp/libreoffice.dmg"
-    user WS_USER
-  end
-
-  execute "copy LibreOffice to /Applications" do
-    command "cp -rf #{Regexp.escape(node["libreoffice_dmg_mnt"]+ "/" + node["libreoffice_basename"])} /Applications/"
-    user WS_USER
-  end
-
-  execute "unmount dmg" do
-    command "hdiutil detach #{Regexp.escape(node["libreoffice_dmg_mnt"])}"
-    user WS_USER
-  end
-
-  ruby_block "test to see if LibreOffice was installed" do
-    block do
-      raise "LibreOffice install failed" unless File.exists?("/Applications/" + node["libreoffice_basename"])
-    end
-  end
+pivotal_workstation_package "LibreOffice" do
+  volumes_dir "LibreOffice 3.3"
+  source "http://download.documentfoundation.org/libreoffice/stable/3.3.4/mac/x86/LibO_3.3.4_MacOS_x86_install_en-US.dmg"
+  checksum "6a6abd0287606fcfedb0ef9bcdf976516a79a09ae81c1734cc82e9ac05b0d7d5"
+  action :install
 end
