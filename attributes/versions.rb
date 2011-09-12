f7c79ab4bc938c4e8325fea5f87894f8deb90991
Brian Cunnie & Matthew Kocher <pair+cunnie+mkocher@pivotallabs.com>
2011-09-12 13:59:53 -0700

    upgraded homebrew to fix libtiff lost server

--- attributes/versions.rb
+++ attributes/versions.rb
@@ -1,5 +1,5 @@
 node.default[:versions]= {
-  "homebrew" => "9b4f9570991160b38c992758e13dd2dfdbdbb0f9",
+  "homebrew" => "11b5b74effd45119816c7bc2710751adbee5f82d",
   "rvm" => "c2a58c6779d19be17a3c3cfddc13e6a0297648b8",
   "Pivotal-Preferences-RubyMine" => "d0eb393ab3d4c92ac80ce3135aa9f71e59cccbd3",
   "RubyMine-VersionString" => "32",
