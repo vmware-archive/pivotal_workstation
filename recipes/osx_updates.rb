b80f368e05528ebcaa08a4041deb4762d92a0299
Matthew Kocher <pair+mkocher@pivotallabs.com>
2011-09-17 15:42:04 -0700

    making software update happen once per hour at most

--- recipes/osx_updates.rb
+++ recipes/osx_updates.rb
@@ -1,3 +1,9 @@
+# This is a theorteically flawed chef recipe.  This installs anything apple thinks
+# is a good idea.  Please understand why you shouldn't use this before using it.
+
+last_update_touchfile = "/tmp/last_software_update"
+
 execute "Check & install all available OS X updates from Apple" do
-  command "softwareupdate -i -a"
+  command "softwareupdate -i -a && touch #{last_update_touchfile}"
+  not_if { File.exists?(last_update_touchfile) && File.new(last_update_touchfile).mtime > Time.now-60*24 }
 end
