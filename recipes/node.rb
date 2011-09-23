7dd9412a6d362537d87a29831980261ebaa1e546
Brian Cunnie & Matthew Kocher <pair+cunnie+mkocher@pivotallabs.com>
2011-09-22 21:00:39 -0700

    re-factored to avoid passing a download directly to shell

--- recipes/node.rb
+++ recipes/node.rb
@@ -1,5 +1,27 @@
 brew_install "node"
 
-execute "install npm" do
-  command "curl http://npmjs.org/install.sh | clean=no sh"
+node_version = "1.0.30"
+
+directory Chef::Config[:file_cache_path] do
+  owner "root"
+  group "admin"
+  mode 0775
+  action :create
+  recursive true
+  not_if { ::File.exists?(Chef::Config[:file_cache_path]) }
+end
+
+npm_git_location = "#{Chef::Config[:file_cache_path]}/npm"
+
+git npm_git_location do
+  repository "https://github.com/isaacs/npm.git"
+  revision "v#{node_version}"
+  action :sync
+  user WS_USER
+end
+
+execute "compile rvm" do
+  command "cd #{npm_git_location} && make clean && make install"
+  user WS_USER
+  not_if "npm -v | grep #{node_version}"
 end
