4ae588bb2a1633e771dc815e021887c87b2db39a
Brian Cunnie & Matthew Kocher <pair+cunnie+mkocher@pivotallabs.com>
2011-09-22 20:24:45 -0700

    upgraded homebrew version

--- attributes/versions.rb
+++ attributes/versions.rb
@@ -1,5 +1,5 @@
 node.default[:versions]= {
-  "homebrew" => "11b5b74effd45119816c7bc2710751adbee5f82d",
+  "homebrew" => "5d680dba45538a9968250046a72f3275a17d86be",
   "rvm" => "c2a58c6779d19be17a3c3cfddc13e6a0297648b8",
   "Pivotal-Preferences-RubyMine" => "d0eb393ab3d4c92ac80ce3135aa9f71e59cccbd3",
   "RubyMine-VersionString" => "32",
