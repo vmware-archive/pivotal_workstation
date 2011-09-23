cf7355cd2366d4038ca457cb7d4ce07efd142a2f
Brian Cunnie & Matthew Kocher <pair+cunnie+mkocher@pivotallabs.com>
2011-09-22 20:24:23 -0700

    unneeded

--- attributes/pg_admin.rb
+++
@@ -1,4 +0,0 @@
-# this link is an html page, even though it uses .dmg; picked a US mirror at random
-# node.default["pg_admin_download_uri"]="http://wwwmaster.postgresql.org/download/mirrors-ftp/pgadmin3/release/v1.12.3/osx/pgadmin3-1.12.3.dmg"
-node.default["pg_admin_download_uri"]="http://wwwmaster.postgresql.org/redir/198/h/pgadmin3/release/v1.12.3/osx/pgadmin3-1.12.3.dmg"
-node.default["pg_admin_app_path"]="/Applications/pgAdmin3.app"
