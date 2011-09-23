7487be133fb8de8a45c706b80c47b1e44b2b5806
Brian Cunnie & Matthew Kocher <pair+cunnie+mkocher@pivotallabs.com>
2011-09-22 20:26:09 -0700

    Much cleaner homebrew recipe

--- recipes/homebrew.rb
+++ recipes/homebrew.rb
@@ -2,19 +2,11 @@ include_recipe "pivotal_workstation::user_owns_usr_local"
 
 homebrew_git_revision_hash  = version_string_for("homebrew")
 
-if (`which brew`.empty?)
-  git "/tmp/homebrew" do
-    repository "https://github.com/mxcl/homebrew.git"
-    revision homebrew_git_revision_hash
-    destination "/tmp/homebrew"
-    action :sync
-    user WS_USER
-  end
-
-  execute "Copying homebrew's .git to /usr/local" do
-    command "rsync -axSH /tmp/homebrew/ /usr/local/"
-    user WS_USER
-  end
+git "/usr/local/" do
+  repository "https://github.com/mxcl/homebrew.git"
+  revision homebrew_git_revision_hash
+  action :sync
+  user WS_USER
 end
 
 ruby_block "Check that homebrew is running & working" do
