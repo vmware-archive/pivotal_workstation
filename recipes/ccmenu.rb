45e0589e05d282a20377b3b0a28d4dfcc4f7ad10
Brian Cunnie & Matthew Kocher <pair+cunnie+mkocher@pivotallabs.com>
2011-09-26 15:50:54 -0700

    ccmenu now has a working link

--- recipes/ccmenu.rb
+++ recipes/ccmenu.rb
@@ -1,6 +1,6 @@
 pivotal_workstation_package "CCMenu" do
   volumes_dir "CCMenu 1.3"
-  source "http://cdnetworks-us-2.dl.sourceforge.net/project/ccmenu/CCMenu/1.3/ccmenu-1.3-b.dmg"
+  source "http://surfnet.dl.sourceforge.net/project/ccmenu/CCMenu/1.3/ccmenu-1.3-b.dmg"
   checksum "9afef887022002b0f074eb7db25349da53aea5d5a64d8415b756c676db9ad1de"
   action :install
 end
