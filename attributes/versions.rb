5a2e249fcfc15ff739fc8c041d52b8bc728d82b5
Brian Cunnie & Matthew Kocher <pair+cunnie+mkocher@pivotallabs.com>
2011-09-26 17:24:44 -0700

    upgraded homebrew, hopefully will fix imagemagick

--- attributes/versions.rb
+++ attributes/versions.rb
@@ -1,5 +1,5 @@
 node.default[:versions]= {
-  "homebrew" => "5d680dba45538a9968250046a72f3275a17d86be",
+  "homebrew" => "65a33c9d1fe8d409d1561ab15190642d4cbfb1f7",
   "rvm" => "c2a58c6779d19be17a3c3cfddc13e6a0297648b8",
   "Pivotal-Preferences-RubyMine" => "d0eb393ab3d4c92ac80ce3135aa9f71e59cccbd3",
   "RubyMine-VersionString" => "32",
