510d7faa85f44587fa0c5f3dc26c3696763780a8
Brian Cunnie & Matthew Kocher <pair+cunnie+mkocher@pivotallabs.com>
2011-09-30 09:50:38 -0700

    We had to go back to the old URL because Skype made their website "more" intelligent, giving us the Windows version of Skype (assumption: they looked at the user-agent & made a bad guess).

--- recipes/skype.rb
+++ recipes/skype.rb
@@ -1,5 +1,5 @@
 pivotal_workstation_package "Skype" do
-  source "http://www.skype.com/go/getskype-macosx.dmg"
+  source "http://download.skype.com/macosx/Skype_5.3.0.1093.dmg"
   action :install
   checksum "ad4f5a8f2312597b98c420045d0828ec40d440fe0da187bde34b7f40294b8e08"
 end
