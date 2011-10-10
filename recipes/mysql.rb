a14c5408579cb1ac305d61286434825a5ca2cc19
Matthew Kocher <pivotal@cocoa.flood.pivotallabs.com>
2011-10-10 11:19:13 -0700

    refactoring mysql recipe a bit, adding time zone tables

--- recipes/mysql.rb
+++ recipes/mysql.rb
@@ -4,13 +4,13 @@ DEFAULT_PIVOTAL_MYSQL_PASSWORD = "password"
 
 include_recipe "pivotal_workstation::homebrew"
 
+directory "/Users/#{WS_USER}/Library/LaunchAgents" do
+  owner WS_USER
+  action :create
+end
+
 run_unless_marker_file_exists("mysql_" + marker_version_string_for("homebrew")) do
   brew_install("mysql", true)
-
-  directory "/Users/#{WS_USER}/Library/LaunchAgents" do
-    owner WS_USER
-    action :create
-  end
 
   execute "copy mysql plist to ~/Library/LaunchAgents" do
     command "cp `brew --prefix mysql`/com.mysql.mysqld.plist #{WS_HOME}/Library/LaunchAgents/"
@@ -21,24 +21,30 @@ run_unless_marker_file_exists("mysql_" + marker_version_string_for("homebrew")) 
     command "mysql_install_db --verbose --user=#{WS_USER} --basedir=\"$(brew --prefix mysql)\" --datadir=/usr/local/var/mysql --tmpdir=/tmp"
     user WS_USER
   end
+end
 
-  execute "load the mysql plist into the mac daemon startup thing" do
-    command "launchctl load -w #{WS_HOME}/Library/LaunchAgents/com.mysql.mysqld.plist"
-    user WS_USER
-  end
+execute "load the mysql plist into the mac daemon startup thing" do
+  command "launchctl load -w #{WS_HOME}/Library/LaunchAgents/com.mysql.mysqld.plist"
+  user WS_USER
+  not_if "launchctl list com.mysql.mysqld"
+end
 
-  ruby_block "wait for mysql to come up" do
-    block do
-      Timeout::timeout(60) do
-        until system("ls /tmp/mysql.sock")
-          sleep 1
-        end
+ruby_block "Checking that mysql is running" do
+  block do
+    Timeout::timeout(60) do
+      until system("ls /tmp/mysql.sock")
+        sleep 1
       end
     end
   end
+end  
 
-  execute "set the root password to the default" do
-    command "mysqladmin -uroot password #{DEFAULT_PIVOTAL_MYSQL_PASSWORD}"
-    not_if "mysql -uroot -p#{DEFAULT_PIVOTAL_MYSQL_PASSWORD} -e 'show databases'"
-  end
+execute "insert time zone info" do
+  command "mysql_tzinfo_to_sql /usr/share/zoneinfo | mysql -uroot -p#{DEFAULT_PIVOTAL_MYSQL_PASSWORD} mysql"
+  not_if "mysql -uroot -ppassword mysql -e 'select * from time_zone_name' | grep -q UTC"
+end
+
+execute "set the root password to the default" do
+  command "mysqladmin -uroot password #{DEFAULT_PIVOTAL_MYSQL_PASSWORD}"
+  not_if "mysql -uroot -p#{DEFAULT_PIVOTAL_MYSQL_PASSWORD} -e 'show databases'"
 end
