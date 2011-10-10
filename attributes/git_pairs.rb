a70b822db44511dd8499146964ec9494a8b38985
Brian Cunnie & Jason Noble <pair+cunnie+jnoble@pivotallabs.com>
2011-10-10 16:51:42 -0700

    Jason Noble is now in the default list

--- attributes/git_pairs.rb
+++ attributes/git_pairs.rb
@@ -1,5 +1,6 @@
 node.default["git_pairs_authors"] = [
   {"initials" => "mk", "name" => "Matthew Kocher", "shortname" => "mkocher"},
   {"initials" => "bc", "name" => "Brian Cunnie", "shortname" => "cunnie"},
+  {"initials" => "jn", "name" => "Jason Noble", "shortname" => "jnoble"},
   {"initials" => "sc", "name" => "Santa Claus"}
   ]
