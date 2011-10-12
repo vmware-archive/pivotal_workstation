ba7ac8aec233be51f2b0bc94973146db72e28e83
Matthew Kocher <pair+mkocher@pivotallabs.com>
2011-10-12 13:53:05 -0700

    Fixing an order of operations problem with mysql, changing the installed rubies to the latest ree and 1.9.2, changing the software updates to only happen once a day instead of once ever 24 minutes

--- attributes/rvm.rb
+++ attributes/rvm.rb
@@ -1,8 +1,8 @@
 node.default["rvm"]= {
   "rubies" => {
       "ree-1.8.7-2011.03" => "CC=gcc-4.2",
-      "ruby-1.8.7-p174" => "CC=gcc-4.2",
-      "ruby-1.9.2-p180" => ""
+      "ruby-1.8.7-p299" => "CC=gcc-4.2",
+      "ruby-1.9.2-p290" => ""
     },
-  "default_ruby" => "ruby-1.9.2-p180"
+  "default_ruby" => "ruby-1.9.2-p290"
   }
