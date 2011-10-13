28915d1426ce9b0fb47b947ecf0dca6bded2b387
Brian Cunnie & Matthew Kocher <pair+cunnie+mkocher@pivotallabs.com>
2011-10-13 10:47:09 -0700

    upgrading homebrew to fix nagging mysql build problem

--- attributes/versions.rb
+++ attributes/versions.rb
@@ -1,5 +1,5 @@
 node.default[:versions]= {
-  "homebrew" => "54a79b4f4b40d8dc8a484a3192aa9fb4193546de",
+  "homebrew" => "3d1a6ef3e896d456b2885bb6456d0114c02aab6f",
   "rvm" => "c2a58c6779d19be17a3c3cfddc13e6a0297648b8",
   "Pivotal-Preferences-RubyMine" => "d0eb393ab3d4c92ac80ce3135aa9f71e59cccbd3",
   "RubyMine-VersionString" => "32",
