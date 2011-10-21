97b67686b7230a51055e06a36825f48b8c2d5373
Brian Cunnie & Rob Head <pair+cunnie+rhead@pivotallabs.com>
2011-10-21 14:26:02 -0700

    Added Rob Head

--- attributes/git_pairs.rb
+++ attributes/git_pairs.rb
@@ -3,5 +3,6 @@ node.default["git_pairs_authors"] = [
   {"initials" => "bc", "name" => "Brian Cunnie", "shortname" => "cunnie"},
   {"initials" => "jn", "name" => "Jason Noble", "shortname" => "jnoble"},
   {"initials" => "rk", "name" => "Reed Kennedy", "shortname" => "reed"},
+  {"initials" => "rh", "name" => "Rob Head", "shortname" => "rhead"},
   {"initials" => "sc", "name" => "Santa Claus"}
   ]
