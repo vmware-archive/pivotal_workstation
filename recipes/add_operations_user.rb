131ac3cd784ab2c67ce5cf15713ac6b57ec6b158
Brian Cunnie & Reed Kennedy <pair+cunnie+reed@pivotallabs.com>
2011-10-26 11:52:01 -0700

    User (operations) add recipe mostly done. Needs to set password. -rk & bc

--- recipes/add_operations_user.rb
+++ recipes/add_operations_user.rb
@@ -1,23 +1,24 @@
-user="operations"
-real_name="Pivotal Operations"
-ruby_block "adding user '#{user}'" do
-  block do
-    system("dscl . -create /Users/#{user}")
-    system("dscl . -create /Users/#{user} UserShell /bin/bash")
-    system("dscl . -create /Users/#{user} RealName \"#{real_name}\"")
-    #check for next sequential unused UID
-    uids = `dscl . -list /Users UniqueID`.split("\n").collect { |pair| pair.squeeze(" ").split(" ").last.to_i }
-    uid = 501
-    while ( uids.include?(uid) ) do
-      uid += 1
-    end
-    puts "I'm gonna use UID #{uid}"
-    system("dscl . -create /Users/#{user} UniqueID \"#{uid}\"")
-    system("dscl . -create /Users/#{user} PrimaryGroupID 20")
-    system("dscl . -create /Users/#{user} NFSHomeDirectory /Users/#{user}")
-    system("dscl . -passwd /Users/#{user} password")
-    system("dscl . -append /Groups/admin GroupMembership #{user}")
-    system("mkdir /Users/#{user}")
-    system("chown #{user}:staff /Users/#{user}")
-  end
+#password_hash = node[:users][:operations][:password_hash] || "!NOLOGIN"
+#password_hash = "!NOLOGIN"
+
+# Find the first available uid > 500
+uids = `dscl . -list /Users UniqueID`.split("\n").collect { |pair| pair.squeeze(" ").split(" ").last.to_i }
+uid = 501
+while ( uids.include?(uid) ) do
+  uid += 1
+end
+
+username="santa"
+user username do
+  comment username.capitalize
+  gid 20
+  uid uid
+  home "/Users/#{username}"
+  shell "/bin/bash"
+  not_if Kernel.system("id santa").to_s
+end
+
+group "admin" do
+  members [username]
+  append true
 end
