aeabb6c520fd48e7b9d877d0f97576843abb4df5
Brian Cunnie & Matthew Kocher <pair+cunnie+mkocher@pivotallabs.com>
2011-10-26 13:28:46 -0700

    re-factored

--- recipes/add_ops_user.rb
+++ recipes/add_ops_user.rb
@@ -1,7 +1,8 @@
 #password_hash = node[:users][:operations][:password_hash] || "!NOLOGIN"
 #password_hash = "!NOLOGIN"
+require 'etc'
 
-# Find the first available uid > 500
+# Find the first available uid > 500 so it shows up in System Preferences
 uids = `dscl . -list /Users UniqueID`.split("\n").collect { |pair| pair.squeeze(" ").split(" ").last.to_i }
 uid = 501
 while ( uids.include?(uid) ) do
@@ -11,12 +12,14 @@ end
 username="ops"
 user username do
   comment username.capitalize
-  gid 20
+  gid Etc.getgrnam("staff").gid 
   uid uid
   home "/Users/#{username}"
   shell "/bin/bash"
-  not_if Kernel.system("id santa").to_s
+  not_if "id #{username}"
 end
+
+execute "defaults write  /var/db/dslocal/nodes/Default/users/#{username} ShadowHashData -array -data \"#{node["ops_user_password_hash"]}\""
 
 group "admin" do
   members [username]
