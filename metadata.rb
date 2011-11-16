cbd8f4615324f403237c2f867bbe88afb2100150
Brian Cunnie & Matthew Kocher <pair+cunnie+mkocher@pivotallabs.com>
2011-11-16 15:11:44 -0800

    Adding metadata

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+maintainer        "Pivotal Labs"
+maintainer_email  "accounts@pivotallabs.com"
+license           "MIT"
+description       "Configure frequently-used tools for an OSX workstation"
+long_description  IO.read(File.join(File.dirname(__FILE__), 'README.markdown'))
+version           "1.0.0"
+supports          "mac_os_x"
