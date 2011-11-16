e246b3b0b960a81931361b0110b72fab815d37e0
Charles Lowell <cowboyd@thefrontside.net>
2011-11-16 09:05:06 -0600

    add metadata so that it can be installed with librarian

---
+++ metadata.rb
@@ -0,0 +1,6 @@
+maintainer        "Pivotal"
+maintainer_email  ""
+license           "Apache 2.0"
+description       "Recipes for an OSX flavored workstation"
+long_description  IO.read File.expand_path('../README.markdown', __FILE__)
+version           "1.0"
