b0bb873417528a3b4fca0dca5b078a4ab87f11a4
Brian Cunnie <cunnie@pivotallabs.com>
2011-12-05 16:58:36 -0800

    Updated firefox to 8.0.1

--- recipes/firefox.rb
+++ recipes/firefox.rb
@@ -1,5 +1,5 @@
 pivotal_workstation_package "Firefox" do
-  source "http://download.mozilla.org/?product=firefox-7.0.1&os=osx&lang=en-US"
+  source "http://download.mozilla.org/?product=firefox-8.0.1&os=osx&lang=en-US"
   action :install
-  checksum "5e040aa64ae9d0af6fae394b447d63db365c1e0c40f8df601575e83ef32ef0c1"
+  checksum "1c93cc36e7ebcaff1e391bd0621fc2a2193ad3bac3454e97ebdd36df84193bf6"
 end
