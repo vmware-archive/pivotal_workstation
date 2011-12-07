9a00e6e2483645734b2788a56b75cc345a93e2a6
Brian Cunnie <cunnie@pivotallabs.com>
2011-12-07 12:10:54 -0800

    Commented out ruby-1.8.7 and ree-1.8.7 because they won't compile correctly under Xcode 4.2 w/out gcc.

--- attributes/rvm.rb
+++ attributes/rvm.rb
@@ -1,7 +1,7 @@
 node.default["rvm"]= {
   "rubies" => {
-      "ree-1.8.7-2011.03" => "CC=gcc-4.2",
-      "ruby-1.8.7-p299" => "CC=gcc-4.2",
+#      "ree-1.8.7-2011.03" => "CC=gcc-4.2",
+#      "ruby-1.8.7-p299" => "CC=gcc-4.2",
       "ruby-1.9.2-p290" => ""
     },
   "default_ruby" => "ruby-1.9.2-p290"
