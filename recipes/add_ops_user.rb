e706263724e5eb4a3ef92990bf0ed01ab6351f2e
Brian Cunnie & Reed Kennedy <pair+cunnie+reed@pivotallabs.com>
2011-12-19 16:44:50 -0800

    Finished recipe to add ops user and set to run automatically

--- recipes/add_ops_user.rb
+++ recipes/add_ops_user.rb
@@ -2,26 +2,37 @@
 #password_hash = "!NOLOGIN"
 require 'etc'
 
-# Find the first available uid > 500 so it shows up in System Preferences
-uids = `dscl . -list /Users UniqueID`.split("\n").collect { |pair| pair.squeeze(" ").split(" ").last.to_i }
-uid = 501
-while ( uids.include?(uid) ) do
-  uid += 1
-end
-
 username="ops"
-user username do
-  comment username.capitalize
-  gid Etc.getgrnam("staff").gid 
-  uid uid
-  home "/Users/#{username}"
-  shell "/bin/bash"
-  not_if "id #{username}"
-end
 
-execute "defaults write  /var/db/dslocal/nodes/Default/users/#{username} ShadowHashData -array -data \"#{node["ops_user_password_hash"]}\""
+unless system("dscl . list /Users/ops")
+  # Find the first available uid > 500 so it shows up in System Preferences
+  uids = `dscl . -list /Users UniqueID`.split("\n").collect { |pair| pair.squeeze(" ").split(" ").last.to_i }
+  uid = 501
+  while ( uids.include?(uid) ) do
+    uid += 1
+  end
 
-group "admin" do
-  members [username]
-  append true
+  user username do
+    comment username.capitalize
+    gid Etc.getgrnam("staff").gid
+    uid uid
+    home "/Users/#{username}"
+    shell "/bin/bash"
+    not_if "id #{username}"
+  end
+
+  #create the user's home directory
+  directory "/Users/#{username}" do
+    owner username
+    group "staff"
+    mode "0755"
+    action :create
+  end
+
+  execute "defaults write  /var/db/dslocal/nodes/Default/users/#{username} ShadowHashData -array -data \"#{node["ops_user_password_hash"]}\""
+
+  group "admin" do
+    members [username]
+    append true
+  end
 end
