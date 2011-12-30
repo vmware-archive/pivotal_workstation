31b5e6892f5593463f4c9a881c29c84052474d10
Pivotal Labs <pivotal@spruce.boulder.pivotallabs.com>
2011-12-30 08:29:22 -0700

    Add Jeff Dean to pairs list

--- attributes/git_pairs.rb
+++ attributes/git_pairs.rb
@@ -2,6 +2,7 @@ node.default["git_pairs_authors"] = [
   {"initials" => "mk", "name" => "Matthew Kocher", "shortname" => "mkocher"},
   {"initials" => "bc", "name" => "Brian Cunnie", "shortname" => "cunnie"},
   {"initials" => "jn", "name" => "Jason Noble", "shortname" => "jnoble"},
+  {"initials" => "jd", "name" => "Jeff Dean", "shortname" => "jdean"},
   {"initials" => "rk", "name" => "Reed Kennedy", "shortname" => "reed"},
   {"initials" => "rh", "name" => "Rob Head", "shortname" => "rhead"},
   {"initials" => "sc", "name" => "Santa Claus"}
