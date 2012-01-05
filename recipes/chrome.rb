1995ffff466ecd189db1007d0dbbf9b765effeaa
Michael Freedman <pair+mfreedman@pivotallabs.com>
2012-01-05 07:30:16 -0800

    Revert "Added user directive to chrome install"
    This reverts commit 604f325fb8a23cb1428fb00f51c86890c6dfe30f.

--- recipes/chrome.rb
+++ recipes/chrome.rb
@@ -3,5 +3,4 @@ pivotal_workstation_package "Google Chrome" do
   source "https://dl-ssl.google.com/chrome/mac/stable/GGRM/googlechrome.dmg"
   checksum "7daa2dc5c46d9bfb14f1d7ff4b33884325e5e63e694810adc58f14795165c91a"
   action :install
-  user WS_USER
 end
