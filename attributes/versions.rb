7ec2b3c1e00da5ef675442e852d753e7a648d727
Wayne Mak & aram price <engineers+wayne+aram@sharethrough.com>
2012-01-20 11:59:46 -0800

    Upgrade rvm to latest sha which supports ree-1.8.7-2010.02 * update pivotal_workstation::rvm to use "pkg" instead of "package" for rvm package installation

--- attributes/versions.rb
+++ attributes/versions.rb
@@ -1,6 +1,6 @@
 node.default[:versions]= {
   "homebrew" => "3d1a6ef3e896d456b2885bb6456d0114c02aab6f",
-  "rvm" => "c2a58c6779d19be17a3c3cfddc13e6a0297648b8",
+  "rvm" => "f94a85e3e217d5667f3c7b275c06dbf44af225f8",
   "Pivotal-Preferences-RubyMine" => "d0eb393ab3d4c92ac80ce3135aa9f71e59cccbd3",
   "RubyMine-VersionString" => "32",
 }
--- recipes/rvm.rb
+++ recipes/rvm.rb
@@ -33,7 +33,7 @@ run_unless_marker_file_exists(marker_version_string_for("rvm")) do
 
   %w{readline autoconf openssl zlib}.each do |rvm_package|
     execute "install rvm package: #{rvm_package}" do
-      command "#{::RVM_COMMAND} package install #{rvm_package}"
+      command "#{::RVM_COMMAND} pkg install #{rvm_package}"
       user WS_USER
     end
   end
