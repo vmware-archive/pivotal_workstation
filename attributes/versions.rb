bbefb17cd2ae076667062ad7af3c5e2cf6a97666
Wayne Mak & aram price <engineers+wayne+aram@sharethrough.com>
2012-01-20 14:51:00 -0800

    Set RVM sha to 1.10.0

--- attributes/versions.rb
+++ attributes/versions.rb
@@ -1,6 +1,6 @@
 node.default[:versions]= {
   "homebrew" => "3d1a6ef3e896d456b2885bb6456d0114c02aab6f",
-  "rvm" => "f94a85e3e217d5667f3c7b275c06dbf44af225f8",
+  "rvm" => "1d4af11da3d870d2e050a6472e386a001a8dea3f",
   "Pivotal-Preferences-RubyMine" => "d0eb393ab3d4c92ac80ce3135aa9f71e59cccbd3",
   "RubyMine-VersionString" => "32",
 }
