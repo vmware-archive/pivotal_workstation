29c884312280b82d41a9bace0f18be2cb5dcc776
Jeff Moore & Stephen Hopkins <pair+jeff+stephen@pivotallabs.com>
2012-01-25 11:55:20 -0800

    supporting rspec 2.8 in rubymine 3

--- recipes/rubymine.rb
+++ recipes/rubymine.rb
@@ -6,3 +6,10 @@ pivotal_workstation_package "Rubymine #{rubymine_version}" do
   action :install
   checksum "5b670894397ae5dcb692bb2b2a7e3b01cedf2e50978879ea4fe1efaa348cbb9c"
 end
+
+# rubymine 4 should not need this
+template "/Applications/RubyMine #{rubymine_version}.app/rb/testing/patch/bdd/teamcity/spec/runner/formatter/teamcity/formatter.rb" do
+  source "rubymine-rspec-2-8-support.erb"
+  owner WS_USER
+  group "admin"
+end
