18a7914f931f2cdfb3e33078ba38419b50da3194
Jacob Maine <jacob@pivotallabs.com>
2012-01-27 22:53:12 -0800

    Make vim recipe work from scratch
    The recipe had many problems.
    
    1. MacVim must be compiled with system ruby.
    2. The not_if blocks were written as if they were run between
    commands, when in reality they are all run before any command.
    This meant that they could only have worked on a subsequent
    run.  Removed many of the not_if blocks... give chef control
    of vi.  Or don't use chef.
    3. The casecommons/vim-config git repo isn't available publicly.
    Using pivotal/vim-config instead.
    4. The multiple commands to download the vim-config repo were
    breaking submodule init.
    5. command-t must be compiled with system ruby.

--- recipes/vim.rb
+++ recipes/vim.rb
@@ -1,48 +1,31 @@
 include_recipe "pivotal_workstation::homebrew"
 include_recipe "pivotal_workstation::ack"
 include_recipe "pivotal_workstation::git"
+include_recipe "pivotal_workstation::rvm"
 
-# https://github.com/mxcl/homebrew/pull/8394
-# default macvim doesn't compile yet with xcode 4.2
-brew_install("macvim", :brew_args => "--HEAD")
+execute "brew install macvim with system ruby" do
+  user WS_USER
+  command "rvm use system; brew install macvim; rvm use default"
+  not_if "brew list | grep '^macvim$'"
+end
 
 # There may be multiple macvims; try to find the latest one
 # & link that to /Applications
 ruby_block "Link MacVim to /Applications" do
   block do
     macvim_app=Dir["/usr/local/Cellar/macvim/*/MacVim.app"].last
+    raise "no macvim found" unless macvim_app
     if File.exists?(macvim_app)
       system("ln -s #{macvim_app} /Applications/")
     end
   end
 end
 
-execute "move existing vim_home out of the way if necessary" do
-  user WS_USER
-  command "mv #{node["vim_home"]} #{WS_HOME}/.vim-old"
-  only_if {
-    File.exists?(node["vim_home"]) && !system("cd #{node["vim_home"]} && git remote -v | grep pivotal/vim-config") && !File.exists?("#{WS_HOME}/.vim-old")
-  }
-end
-
 git "#{node["vim_home"]}" do
-  only_if { system("test ! -d #{node["vim_home"]}") }
-  repository "git@github.com:casecommons/vim-config.git"
+  repository "git://github.com/pivotal/vim-config.git"
   branch "master"
   revision node["vim_hash"] || "HEAD"
   action :checkout
-  user WS_USER
-  enable_submodules true
-end
-
-git "#{node["vim_home"]}" do
-  only_if {
-    system("test ! -d #{node["vim_home"]} || cd #{node["vim_home"]} && git remote -v | grep pivotal/vim-config")
-  }
-  repository "git@github.com:casecommons/vim-config.git"
-  branch "master"
-  revision node["vim_hash"] || "HEAD"
-  action :sync
   user WS_USER
   enable_submodules true
 end
@@ -55,15 +38,15 @@ end
 brew_install "ctags"
 
 execute "compile command-t" do
-  only_if "test -d #{node["vim_home"]}/bundle/command-t/ruby/command-t"
+  only_if "test ! -f #{node["vim_home"]}/bundle/command-t/ruby/command-t/compiled"
   cwd "#{node["vim_home"]}/bundle/command-t/ruby/command-t"
-  command "ruby extconf.rb && make"
+  command "rvm use system && ruby extconf.rb && make && touch compiled && rvm use default"
   user WS_USER
 end
 
 ruby_block "test to see if MacVim link worked" do
   block do
-    raise "/Applications/MacVim install failed" unless File.exists?("/Applications/MacVim.app/Contents")
+    raise "/Applications/MacVim install failed" unless File.exists?("/Applications/MacVim.app")
   end
 end
 
