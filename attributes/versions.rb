c9d367db5736e8281a166e917ef12be810ab1cff
Brian Cunnie <cunnie@pivotallabs.com>
2012-01-30 11:04:21 -0800

    Updated homebrew version to pull in version 2.8.7 of cmake

--- attributes/versions.rb
+++ attributes/versions.rb
@@ -1,5 +1,5 @@
 node.default[:versions]= {
-  "homebrew" => "3d1a6ef3e896d456b2885bb6456d0114c02aab6f",
+  "homebrew" => "6c7f2d822d4dff6aff57474cc816b91a7474b31b",
   "rvm" => "869e226a8739dbc31dd958ff331d8b0a6f11d759",
   "Pivotal-Preferences-RubyMine" => "d0eb393ab3d4c92ac80ce3135aa9f71e59cccbd3",
   "RubyMine-VersionString" => "32",
