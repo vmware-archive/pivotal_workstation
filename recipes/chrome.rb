e15e139c842d30ca45205ef49cfb3522347c5a18
Brian Cunnie <cunnie@pivotallabs.com>
2012-01-30 14:37:15 -0800

    updated checksums for newer images

--- recipes/chrome.rb
+++ recipes/chrome.rb
@@ -1,6 +1,6 @@
 pivotal_workstation_package "Google Chrome" do
   dmg_name "googlechrome"
   source "https://dl-ssl.google.com/chrome/mac/stable/GGRM/googlechrome.dmg"
-  checksum "7daa2dc5c46d9bfb14f1d7ff4b33884325e5e63e694810adc58f14795165c91a"
+  checksum "bd85e06643abc3af5b1a049d8031c29f339f456c2d25852a7a4a1e6578b49792"
   action :install
 end
