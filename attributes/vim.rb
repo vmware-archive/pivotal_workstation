1a196c26b5810292a5476e92fcca09859929af9b
Jacob Maine <jacob@pivotallabs.com>
2012-02-04 22:34:05 -0800

    vim command-t is back

--- attributes/vim.rb
+++ attributes/vim.rb
@@ -1,1 +1,2 @@
 node.default["vim_home"]="#{WS_HOME}/.vim"
+node.default["vim_hash"]="1721ef51e99af35e4f9210eaa1ca536c955a4067"
--- recipes/vim.rb
+++ recipes/vim.rb
@@ -37,6 +37,13 @@ end
 
 brew_install "ctags"
 
+execute "compile command-t" do
+  not_if "test -f #{node["vim_home"]}/bundle/command-t/ruby/command-t/compiled"
+  cwd "#{node["vim_home"]}/bundle/command-t/ruby/command-t"
+  command "rvm use system && ruby extconf.rb && make && touch compiled"
+  user WS_USER
+end
+
 ruby_block "test to see if MacVim link worked" do
   block do
     raise "/Applications/MacVim install failed" unless File.exists?("/Applications/MacVim.app")
