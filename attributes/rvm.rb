7486815a08173832ce8ce4634f7acf0063094487
Brian Cunnie <cunnie@pivotallabs.com>
2012-02-27 18:09:11 -0800

    commented out rvm's default build of 1.9.2-p290 with clang; clang was unsupported, and it would be onerous to work around [possibly spurious] error messages indicating failure of the build

--- attributes/rvm.rb
+++ attributes/rvm.rb
@@ -4,7 +4,7 @@ node.default["rvm"]= {
       # as it overwrites later versions of files provided by xcode.
     #"ree-1.8.7-2012.02" => { :env => "CC=gcc-4.2" },
     #"ruby-1.8.7-p358" => { :env => "CC=gcc-4.2" },
-    "ruby-1.9.2-p290" => { :command_line_options => "--with-gcc=clang" },
+    #"ruby-1.9.2-p290" => { :env => "CC=gcc-4.2" },
     "ruby-1.9.3-p125" => { :command_line_options => "--with-gcc=clang" },
   },
   "default_ruby" => "ruby-1.9.2-p290"
