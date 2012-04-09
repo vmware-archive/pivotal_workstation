fe526f121371f3ea76a178e60c4ec6aa43f7411b
Brian Cunnie <pair+cunnie@pivotallabs.com>
2012-04-09 12:02:47 -0700

    rvm is moving to rvm.io; causes these problems: http://stackoverflow.com/questions/10050972/rvm-refusing-to-install-and-giving-non-existant-log-files; this should fix

--- attributes/versions.rb
+++ attributes/versions.rb
@@ -1,6 +1,6 @@
 node.default[:versions]= {
   "homebrew" => "36aa3c27aac6c673f409aef2da921137b2ef7967",
-  "rvm" => "45a40eaa24c1d89268746e8de2913290afd9af42",
+  "rvm" => "b8c97c1020f86e76eb5862f8a603a58d56060a93",
   "Pivotal-Preferences-RubyMine" => "d0eb393ab3d4c92ac80ce3135aa9f71e59cccbd3",
   "RubyMine-VersionString" => "32",
 }
