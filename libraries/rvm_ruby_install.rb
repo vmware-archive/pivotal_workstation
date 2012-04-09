f86e4583f9b7a3988a2b7daef8b9f2fce6f83b34
Michael Schubert & Trace Wax <pair+mschubert+trace@pivotallabs.com>
2012-04-09 17:04:33 -0400

    Ignore warning reported as an error by rvm when compiling yaml using clang

--- libraries/rvm_ruby_install.rb
+++ libraries/rvm_ruby_install.rb
@@ -18,7 +18,7 @@ class Chef::Recipe
       #| (! grep 'error') : if we see rvm errors in stderr, fail
       #this is due to an rvm bug (we've notified the author).  as soon as curl error cause rvm to exit nonzero,
       #we can get rid of this
-      install_cmd << " 2>&1 | (! grep error)"
+      install_cmd << " 2>&1 | (! grep error | grep -v \"clang: error: unsupported option '--with-libyaml'\")"
 
       execute "installing #{ruby_version} with RVM: #{install_cmd}" do
         command install_cmd
