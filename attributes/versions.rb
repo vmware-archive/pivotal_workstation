bc8a33955cd7ec3f63752b40bad40e9ff399c89d
Patrick Leahy & Tim Labeeuw <pair+pleahy+tlabeeuw@pivotallabs.com>
2012-04-20 11:19:54 -0700

    bumping rvm

--- attributes/versions.rb
+++ attributes/versions.rb
@@ -1,6 +1,6 @@
 node.default[:versions]= {
   "homebrew" => "36aa3c27aac6c673f409aef2da921137b2ef7967",
-  "rvm" => "45a40eaa24c1d89268746e8de2913290afd9af42",
+  "rvm" => "85e0d3e8d1e63e7620335c38c755a63a4c134487",
   "Pivotal-Preferences-RubyMine" => "d0eb393ab3d4c92ac80ce3135aa9f71e59cccbd3",
   "RubyMine-VersionString" => "32",
 }
