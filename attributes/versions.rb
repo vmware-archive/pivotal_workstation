ddc4248e730cbbcffdb66b3db55b8ef2275a3eeb
Brian Cunnie & Matthew Kocher <pair+cunnie+mkocher@pivotallabs.com>
2012-05-11 19:02:54 -0700

    Updated LibreOffice, Homebrew, Rubymine preferences, and TextMate download location

--- attributes/versions.rb
+++ attributes/versions.rb
@@ -1,6 +1,4 @@
 node.default['versions']= {
-  "homebrew" => "36aa3c27aac6c673f409aef2da921137b2ef7967",
-  "rvm" => "85e0d3e8d1e63e7620335c38c755a63a4c134487",
-  "Pivotal-Preferences-RubyMine" => "d0eb393ab3d4c92ac80ce3135aa9f71e59cccbd3",
-  "RubyMine-VersionString" => "32",
+  "homebrew" => "b73b40fa1da84411cd7089116da1b20d55f0208d",
+  "rvm" => "85e0d3e8d1e63e7620335c38c755a63a4c134487"
 }
