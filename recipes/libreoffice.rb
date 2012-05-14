337cc545742f829c33630d5b9064e74d24a1aa75
Brian Cunnie <brian.cunnie@gmail.com>
2012-05-13 17:42:20 -0700

    bumped up LibreOffice to 3.5.3

--- recipes/libreoffice.rb
+++ recipes/libreoffice.rb
@@ -1,5 +1,5 @@
-node.default["libreoffice"]["source"] = "http://download.documentfoundation.org/libreoffice/stable/3.5.2/mac/x86/LibO_3.5.2_MacOS_x86_install_en-US.dmg"
-node.default["libreoffice"]["checksum"] = "8d43111f2ddc93aced890d14cb20b6d5f74b5a5cf99badbb6d3da12b959382b8"
+node.default["libreoffice"]["source"] = "http://download.documentfoundation.org/libreoffice/stable/3.5.3/mac/x86/LibO_3.5.3_MacOS_x86_install_en-US.dmg"
+node.default["libreoffice"]["checksum"] = "fb96e2c6a87eb36aae000829041ddfbcc129d360610c2e450e57f89e102fef3e"
 
 pivotal_workstation_package "LibreOffice" do
   volumes_dir "LibreOffice"
