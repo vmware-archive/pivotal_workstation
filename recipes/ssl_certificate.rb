080da7bcec0e2c8b1251707352dbded1af7acd8b
Brian Cunnie & Matthew Kocher <pair+cunnie+mkocher@pivotallabs.com>
2012-05-18 14:20:27 -0700

    refactored to meet FoodCritic's stringent standards

--- recipes/ssl_certificate.rb
+++ recipes/ssl_certificate.rb
@@ -9,42 +9,22 @@ run_unless_marker_file_exists("ssl_certificate") do
     end
   end
 
-  execute "create database.txt" do
-    command "touch #{ca_path}/index.txt"
-    user WS_USER
-  end
-
-  execute "create serial" do
-    command "echo '01\n' > #{ca_path}/serial"
-    user WS_USER
-  end
-
-  execute "generate key" do
-    command "openssl genrsa -des3 -passout pass:password -out #{ca_path}/keys/ca.key 1024"
-    user WS_USER
-  end
-
-  execute "generate ca" do
-    command "cd #{ca_path} && openssl req -new -x509 -days 1001 -key #{ca_path}/keys/ca.key -passin pass:password -out #{ca_path}/certs/ca.cert -subj '/CN=mydomain.com/OU=Org Unit/O=My Org Pty Ltd/L=Sydney/ST=NSW/C=AU/emailAddr=someone@example.com'"
-    user WS_USER
+  ["touch #{ca_path}/index.txt",
+   "echo '01\n' > #{ca_path}/serial",
+   "openssl genrsa -des3 -passout pass:password -out #{ca_path}/keys/ca.key 1024",
+   "cd #{ca_path} && openssl req -new -x509 -days 1001 -key #{ca_path}/keys/ca.key -passin pass:password -out #{ca_path}/certs/ca.cert -subj '/CN=mydomain.com/OU=Org Unit/O=My Org Pty Ltd/L=Sydney/ST=NSW/C=AU/emailAddr=someone@example.com'",
+  ].each do |ssl_cmd|
+    command ssl_cmd
   end
 
   execute "trust our CA" do
     command "security add-trusted-cert -d -r trustRoot -k /Library/Keychains/System.keychain #{ca_path}/certs/ca.cert"
   end
 
-  execute "generate server key" do
-    command "openssl genrsa 1024 > #{cert_path}/server.key"
-    user WS_USER
-  end
-
-  execute "generate request" do
-    command "openssl req -new -key #{cert_path}/server.key -out #{cert_path}/request.csr -subj '/CN=#{node["ssl_settings"]["common_name"]}/OU=Org Unit/O=My Org Pty Ltd/L=Sydney/ST=NSW/C=AU/emailAddr=someoneATexample.com'"
-    user WS_USER
-  end
-
-  execute "sign request with CA" do
-    command "cd #{cert_path} && openssl ca -policy policy_anything -cert #{ca_path}/certs/ca.cert -batch -in #{cert_path}/request.csr -passin pass:password  -keyfile #{ca_path}/keys/ca.key -days 9999 -out #{cert_path}/signed.cert"
-    user WS_USER
+  ["openssl genrsa 1024 > #{cert_path}/server.key",
+   "openssl req -new -key #{cert_path}/server.key -out #{cert_path}/request.csr -subj '/CN=#{node["ssl_settings"]["common_name"]}/OU=Org Unit/O=My Org Pty Ltd/L=Sydney/ST=NSW/C=AU/emailAddr=someoneATexample.com'",
+   "cd #{cert_path} && openssl ca -policy policy_anything -cert #{ca_path}/certs/ca.cert -batch -in #{cert_path}/request.csr -passin pass:password  -keyfile #{ca_path}/keys/ca.key -days 9999 -out #{cert_path}/signed.cert",
+  ].each do |ssl_cmd|
+    command ssl_cmd
   end
 end
