f2ceef38ec744bd81cccc14bd4774e1501ae8c75
Brian Cunnie & Matthew Kocher <pair+cunnie+mkocher@pivotallabs.com>
2012-05-18 15:09:33 -0700

    Fix java, now a 'pkg'

--- recipes/java.rb
+++ recipes/java.rb
@@ -2,7 +2,7 @@ pivotal_workstation_package "JavaForMacOSX10.7" do
   source "http://support.apple.com/downloads/DL1421/en_US/JavaForMacOSX10.7.dmg"
   volumes_dir "Java for Mac OS X 10.7"
   action :install
-  type "mpkg"
+  type "pkg"
   package_id "com.apple.pkg.JavaForMacOSX107"
   checksum "a1b493280c0854f5b7357dab00115db2db48df6463ba7c53c5fd10675c066783"
 end
