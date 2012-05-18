7b4f36c0988f0e48b9f68df5643fd34f3a36dcba
Brian Cunnie & Matthew Kocher <pair+cunnie+mkocher@pivotallabs.com>
2012-05-18 14:39:59 -0700

    refactored to foodcritic's criticism

--- recipes/rvm.rb
+++ recipes/rvm.rb
@@ -17,7 +17,7 @@ run_unless_marker_file_exists(marker_version_string_for("rvm")) do
 ["curl -Lsf http://github.com/wayneeseguin/rvm/tarball/#{rvm_git_revision_hash} | tar xvz -C#{RVM_HOME}/src/rvm --strip 1",
  "cd #{RVM_HOME}/src/rvm; ./install",
  "#{RVM_COMMAND} --version | grep Wayne"].each do |rvm_cmd|
-    command rvm_cmd
+    execute rvm_cmd
       user WS_USER
     end
   end
