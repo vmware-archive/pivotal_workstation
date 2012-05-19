838a20329d2457808419ddb07b49636b00c7323b
Brian Cunnie & Matthew Kocher <pair+cunnie+mkocher@pivotallabs.com>
2012-05-18 18:14:23 -0700

    Cleanup of package provider & timemachine preferences

--- recipes/timemachine_preferences.rb
+++ recipes/timemachine_preferences.rb
@@ -3,17 +3,19 @@ execute "TimeMachine should NOT ask to use every new disk"   do
   user WS_USER
 end
 
-ruby_block "TimeMachine should NOT appear in the status bar" do
-  block do
-    new_menuExtras=""
-    `sudo -u #{WS_USER} defaults read com.apple.systemuiserver menuExtras`.split("\n").each do |line|
-      if line !~ /TimeMachine.menu/
-	new_menuExtras = new_menuExtras + line + "\n"
-      end
-    end
-    `sudo -u #{WS_USER} defaults write com.apple.systemuiserver menuExtras \'#{new_menuExtras}\'`
-    `killall -HUP SystemUIServer`
-  end
+old_menu_extras = `sudo -u #{WS_USER} defaults read com.apple.systemuiserver menuExtras`
+new_menu_extras = old_menu_extras.split("\n").select { |line| line !~ /TimeMachine.menu/ }.join("\n")
+
+execute "TimeMachine should NOT appear in the status bar" do
+  command "defaults write com.apple.systemuiserver menuExtras \'#{new_menu_extras}\'"
+  user WS_USER
+  only_if { new_menu_extras != old_menu_extras }
+  notifies :run, "execute[restart SustemUIServer]"
+end
+
+execute "restart SustemUIServer" do
+  command "killall -HUP SystemUIServer"
+  action :nothing
 end
 
 execute "make sure it doesn't exist"   do
