b09529da60a011a88eaf6cfffa6272fdd3d087f8
Brian Cunnie & Matthew Kocher <pair+cunnie+mkocher@pivotallabs.com>
2012-05-18 17:38:41 -0700

    Added recipe to create chef's cache dir (if non-existent)

---
+++ recipes/create_var_chef_cache.rb
@@ -0,0 +1,10 @@
+# Solo doesn't necessarily create the file_cache_path,
+# but we don't want to mess with it if it exists
+directory Chef::Config[:file_cache_path] do
+  owner "root"
+  group "admin"
+  mode 0775
+  action :create
+  recursive true
+  not_if { ::File.exists?(Chef::Config[:file_cache_path]) }
+end
