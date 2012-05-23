b1f973ee6c04f890d4a87724659653ef80ef3f2a
Brian Cunnie & Matthew Kocher <pair+cunnie+mkocher@pivotallabs.com>
2012-05-22 18:11:43 -0700

    sizeup starts on login

--- recipes/sizeup.rb
+++ recipes/sizeup.rb
@@ -12,6 +12,12 @@ unless File.exists?("/Applications/SizeUp.app")
     mode "0444"
   end
 
+  # start up on login
+  execute "Start SizeUp automatically" do
+    command "defaults write loginwindow AutoLaunchedApplicationDictionary -array-add '{ \"Path\" = \"/Applications/SizeUp.app\"; \"Hide\" = 0; }'"
+    user WS_USER
+  end
+
   execute "unzip sizeup" do
     command "unzip #{Chef::Config[:file_cache_path]}/sizeup.zip SizeUp.app/* -d /Applications/"
     user WS_USER
