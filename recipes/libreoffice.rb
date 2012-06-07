e7ec79bec12b8f3e57a53d435eacfa135af7d6a6
Brian Cunnie & Nick Seemiller <pair+cunnie+seemiller@pivotallabs.com>
2012-06-07 10:35:38 -0700

    Update LibreOffice to version 3.5.4

--- recipes/libreoffice.rb
+++ recipes/libreoffice.rb
@@ -1,5 +1,5 @@
-node.default["libreoffice"]["source"] = "http://download.documentfoundation.org/libreoffice/stable/3.5.3/mac/x86/LibO_3.5.3_MacOS_x86_install_en-US.dmg"
-node.default["libreoffice"]["checksum"] = "fb96e2c6a87eb36aae000829041ddfbcc129d360610c2e450e57f89e102fef3e"
+node.default["libreoffice"]["source"] = "http://download.documentfoundation.org/libreoffice/stable/3.5.4/mac/x86/LibO_3.5.4_MacOS_x86_install_en-US.dmg"
+node.default["libreoffice"]["checksum"] = "b4302f30f6aafe384f13673aaba05ff85c1b4617f997c22e1079df48c6172fee"
 
 pivotal_workstation_package "LibreOffice" do
   volumes_dir "LibreOffice"
