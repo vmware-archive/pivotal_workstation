3f1018238b93bcaea0b058d5df419c26c7c6ce02
Michael Freedman & Michael Gamba <pair+mfreedman+mike@pivotallabs.com>
2012-06-14 15:54:38 -0400

    added guard for front row disable recipe

--- recipes/disable_front_row.rb
+++ recipes/disable_front_row.rb
@@ -1,4 +1,5 @@
 execute "move front row out of the way" do
   command "mv /System/Library/CoreServices/Front\\ Row.app /System/Library/CoreServices/Front_Row_disabled.app"
   not_if { File.exists?("/System/Library/CoreServices/Front_Row_disabled.app") }
+  only_if { File.exists?("/System/Library/CoreServices/Front\\ Row.app") }
 end
