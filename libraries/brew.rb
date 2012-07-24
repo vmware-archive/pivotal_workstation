ed0d95b3755167747de466541a194516feab2a82
pivotal <pivotal@depew.(none)>
2012-07-24 13:31:43 -0700

    Fixing plists, fixing noise brew output when brew has not been installed yet

--- libraries/brew.rb
+++ libraries/brew.rb
@@ -8,14 +8,13 @@ class Chef::Recipe
 
   def brew_installed?(package)
     include_recipe "pivotal_workstation::homebrew"
-
-    installed=(system("brew list #{package} > /dev/null") || brew_has_multiple_versions_installed?(package))
+    installed=(system("brew list #{package} > /dev/null 2>&1") || brew_has_multiple_versions_installed?(package))
     Chef::Log.debug("brew package #{package} " + (installed ? "IS" : "IS NOT") + " installed.")
     installed
   end
 
   def brew_has_multiple_versions_installed?(package)
-    multiple=system("brew list #{package} | grep -q '#{package} has multiple installed versions'")
+    multiple=system("brew list #{package} 2>/dev/null | grep -q '#{package} has multiple installed versions'")
     Chef::Log.debug("brew package #{package} " + (multiple ? "HAS" : "does NOT HAVE") + " multiple versions.")
     multiple
   end
