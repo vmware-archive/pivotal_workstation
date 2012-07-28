93f7905476c36702f4fef93518d74dff05700a9c
Ben Curren <ben@outright.com>
2012-07-27 22:53:21 -0700

    updated sha1 to be 256 bit

--- recipes/evernote.rb
+++ recipes/evernote.rb
@@ -1,7 +1,7 @@
 pivotal_workstation_package "Evernote" do
   volumes_dir "Evernote"
   source "http://evernote.com/download/get.php?file=EvernoteMac"
-  checksum "2af4e1169b384c52ce23d22f284d48d50621d3c8"
+  checksum "20bf4ac0ed5e0e3c80806a60b72c986f9407ccedcd5b763c88a0440ac11db9b6"
   accept_eula true
   action :install
 end
