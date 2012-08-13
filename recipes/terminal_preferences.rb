58fa958e22c4469acdbe6875a90a15f6830d7f3f
Brian Cunnie <pair+cunnie@pivotallabs.com>
2012-08-13 11:15:56 -0700

    Now sets the scheme for "default" as well as "startup".
    Thanks Winston Teo!

--- recipes/terminal_preferences.rb
+++ recipes/terminal_preferences.rb
@@ -1,5 +1,11 @@
 pivotal_workstation_defaults "Set terminal color scheme to #{node['terminal']['color_scheme']}" do
   domain 'com.apple.Terminal'
+  key 'Default Window Settings'
+  string node['terminal']['color_scheme']
+end
+
+pivotal_workstation_defaults "Set startup terminal color scheme to #{node['terminal']['color_scheme']}" do
+  domain 'com.apple.Terminal'
   key 'Startup Window Settings'
   string node['terminal']['color_scheme']
 end
