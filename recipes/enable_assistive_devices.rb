d6593ca141b0eaebc33200b099226ac553ab8a38
Brian Cunnie <pair+cunnie@pivotallabs.com>
2012-08-17 15:34:40 -0700

    Enabling Assistive Devices gives security warning

--- recipes/enable_assistive_devices.rb
+++ recipes/enable_assistive_devices.rb
@@ -2,3 +2,6 @@ execute "Enable Assistive Devices" do
   command "echo 'a' | sudo tee /private/var/db/.AccessibilityAPIEnabled"
   user WS_USER
 end
+
+log_msg="'Enable Assistive Devices' has been turned on.\nThis makes your system less secure.  To disable,\nSystem Preferences -> Accessibility -> (uncheck) Enable Assistive Devices"
+log(log_msg) { level :warn }
