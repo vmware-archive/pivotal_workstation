cc84e48bea1ebd00aa6f35a920d8d1279eba5dc0
Brian Cunnie <pair+cunnie@pivotallabs.com>
2012-08-17 15:56:26 -0700

    enable_assistive_devices now called from sizeup

--- recipes/enable_assistive_devices.rb
+++ recipes/enable_assistive_devices.rb
@@ -1,7 +1,11 @@
-execute "Enable Assistive Devices" do
-  command "echo 'a' | sudo tee /private/var/db/.AccessibilityAPIEnabled"
-  user WS_USER
+# May not work if System Preferences is open while this is running
+file "/private/var/db/.AccessibilityAPIEnabled" do
+  action :create_if_missing
+  content "a"
+  mode "0444"
 end
 
-log_msg="'Enable Assistive Devices' has been turned on.\nThis makes your system less secure.  To disable,\nSystem Preferences -> Accessibility -> (uncheck) Enable Assistive Devices"
+log_msg="'Enable Assistive Devices' has been turned on. This makes your system less secure
+(especially with regards to keylogging). To disable, do the following:
+System Preferences -> Accessibility -> (uncheck) Enable Assistive Devices"
 log(log_msg) { level :warn }
