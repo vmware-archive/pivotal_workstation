7f629cedbc7f095e6c35b41d82b68f26861f76ef
Brian Cunnie <pair+cunnie@pivotallabs.com>
2012-08-20 15:57:41 -0700

    mysql directories now owned by WS_USER.
    Previously, this recipe created /usr/local/var owned root; now it's
    owned by WS_USER.  This had caused problems for the postgres recipe.'

--- recipes/mysql.rb
+++ recipes/mysql.rb
@@ -1,13 +1,20 @@
 #http://solutions.treypiepmeier.com/2010/02/28/installing-mysql-on-snow-leopard-using-homebrew/
 require 'pathname'
 
-DEFAULT_PIVOTAL_MYSQL_PASSWORD = "password"
+PASSWORD = "password"
+# The next two directories will be owned by WS_USER
+DATA_DIR = "/usr/local/var/mysql"
+PARENT_DATA_DIR = "/usr/local/var"
 
 include_recipe "pivotal_workstation::homebrew"
 
-directory "/Users/#{WS_USER}/Library/LaunchAgents" do
-  owner WS_USER
-  action :create
+[ "/Users/#{WS_USER}/Library/LaunchAgents",
+  PARENT_DATA_DIR,
+  DATA_DIR ].each do |dir|
+  directory dir do
+    owner WS_USER
+    action :create
+  end
 end
 
 brew_install("mysql")
@@ -26,7 +33,7 @@ ruby_block "mysql_install_db" do
     active_mysql = Pathname.new("/usr/local/bin/mysql").realpath
     basedir = (active_mysql + "../../").to_s
     data_dir = "/usr/local/var/mysql"
-    system("mysql_install_db --verbose --user=#{WS_USER} --basedir=#{basedir} --datadir=#{data_dir} --tmpdir=/tmp && chown #{WS_USER} #{data_dir}") || raise("Failed initializing mysqldb")
+    system("mysql_install_db --verbose --user=#{WS_USER} --basedir=#{basedir} --datadir=#{DATA_DIR} --tmpdir=/tmp && chown #{WS_USER} #{data_dir}") || raise("Failed initializing mysqldb")
   end
   not_if { File.exists?("/usr/local/var/mysql/mysql/user.MYD")}
 end
@@ -48,11 +55,11 @@ ruby_block "Checking that mysql is running" do
 end
 
 execute "set the root password to the default" do
-  command "mysqladmin -uroot password #{DEFAULT_PIVOTAL_MYSQL_PASSWORD}"
-  not_if "mysql -uroot -p#{DEFAULT_PIVOTAL_MYSQL_PASSWORD} -e 'show databases'"
+  command "mysqladmin -uroot password #{PASSWORD}"
+  not_if "mysql -uroot -p#{PASSWORD} -e 'show databases'"
 end
 
 execute "insert time zone info" do
-  command "mysql_tzinfo_to_sql /usr/share/zoneinfo | mysql -uroot -p#{DEFAULT_PIVOTAL_MYSQL_PASSWORD} mysql"
-  not_if "mysql -uroot -p#{DEFAULT_PIVOTAL_MYSQL_PASSWORD} mysql -e 'select * from time_zone_name' | grep -q UTC"
+  command "mysql_tzinfo_to_sql /usr/share/zoneinfo | mysql -uroot -p#{PASSWORD} mysql"
+  not_if "mysql -uroot -p#{PASSWORD} mysql -e 'select * from time_zone_name' | grep -q UTC"
 end
