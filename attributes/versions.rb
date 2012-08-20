9201d52b088c2559a780cecb466766db17835836
Engineer @wrap <engineer@wrap.local>
2012-08-20 10:05:42 -0700

    bash-it recipe, attributes, and version
    * a clone of oh-my-zsh https://github.com/revans/bash-it/
    * this is a potential replacement for bash_profile_include
    * it includes git integration replacing bash_profile-git_completion.sh.erb

--- attributes/versions.rb
+++ attributes/versions.rb
@@ -1,4 +1,5 @@
 node.default['versions']= {
+  "bash_it" => "5cb0ecc1c813bc5619e0f708b8015a4596a37d6c",
   "homebrew" => "bf4f9aa736d466e12f33b3100a1859e1a1a3d99a",
   "rvm" => "85e0d3e8d1e63e7620335c38c755a63a4c134487"
 }
