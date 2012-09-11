d0802ebff0afc0d5adf49e7089030f654f658b6e
seemiller <seemiller@pivotallabs.com>
2012-09-10 22:15:55 -0600

    update java to 2012-05

--- recipes/java.rb
+++ recipes/java.rb
@@ -1,8 +1,8 @@
 pivotal_workstation_package "JavaForOSX" do
-  source "http://support.apple.com/downloads/DL1515/en_US/"
-  volumes_dir "Java for OS X 2012-004"
+  source "http://support.apple.com/downloads/DL1572/en_US/"
+  volumes_dir "Java for OS X 2012-005"
   action :install
   type "pkg"
   package_id "com.apple.pkg.JavaForMacOSX107"
-  checksum "817ea23add35a71b9b217a05eeb7862cd3b815246b1bfca35ba54480181362fd"
+  checksum "e0750c72972b8a2ccbcb3144bb31d74419276387"
 end
