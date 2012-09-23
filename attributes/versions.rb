b586d102737005b321bd4520960f2acebe2b4828
Brian Cunnie <brian.cunnie@gmail.com>
2012-09-23 08:32:39 -0700

    update homebrew to bring in more robust ossp-uuid download site (required by postgres)
    Fixes #93

--- attributes/versions.rb
+++ attributes/versions.rb
@@ -1,4 +1,4 @@
 node.default['versions']= {
-  "homebrew" => "bf4f9aa736d466e12f33b3100a1859e1a1a3d99a",
+  "homebrew" => "4b7554ccd9b7cdc2570d3586871fdf612c40992f",
   "rvm" => "85e0d3e8d1e63e7620335c38c755a63a4c134487"
 }
