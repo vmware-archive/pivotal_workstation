2278f30fc86a3e4cc216e79be49d7d49d52d0c49
Brian Cunnie <pair+cunnie@pivotallabs.com>
2012-09-28 15:34:09 -0700

    vagrant: add 1.0.5

---
+++ recipes/vagrant.rb
@@ -0,0 +1,9 @@
+include_recipe "pivotal_workstation::virtualbox"
+
+pivotal_workstation_package "Vagrant" do
+  source "http://files.vagrantup.com/packages/be0bc66efc0c5919e92d8b79e973d9911f2a511f/Vagrant-1.0.5.dmg"
+  checksum "d9ccdd454389f5830a8218c066c8f54c15d9d32ca6060bc42677b495aad08003"
+  action :install
+  type "pkg"
+  package_id "com.vagrant.vagrant"
+end
