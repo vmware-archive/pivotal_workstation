55be495aa99d98c34f594a5d9d5bfd9829204a7f
Brian Cunnie <pair+cunnie@pivotallabs.com>
2012-10-09 17:01:58 -0700

    rubymine.rb: bumped to 4.5.4; moved version num into recipe

--- attributes/rubymine.rb
+++ attributes/rubymine.rb
@@ -1,6 +1,3 @@
 node.default["rubymine"]= {
-    :major => "4",
-    :minor => "0",
-    :micro => "3",
     :pivotal_preferences_hash => "b4e225890597302db8d02fd8dba6769ab28af4cd"
 }
--- recipes/rubymine.rb
+++ recipes/rubymine.rb
@@ -1,9 +1,7 @@
 include_recipe "pivotal_workstation::java"
 
-rubymine_version  = "#{node['rubymine']['major']}.#{node['rubymine']['minor']}.#{node['rubymine']['micro']}"
-
 pivotal_workstation_package "RubyMine" do
-  source "http://download.jetbrains.com/ruby/RubyMine-#{rubymine_version}.dmg"
+  source "http://download.jetbrains.com/ruby/RubyMine-4.5.4.dmg"
   action :install
-  checksum "1e5974b07e2cf20ac6156cdb8c1439b2dfde1926233f390f18def01b1d144d4f"
+  checksum "1983d4417c6b2817bd6534f8804c2366fbd074a17dbc5a98cd85161abe255714"
 end
