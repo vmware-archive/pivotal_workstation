36ca809ca80cf4861d5b0344f9a7793f49d2a25b
Brian Cunnie <pair+cunnie@pivotallabs.com>
2012-10-11 12:48:20 -0700

    vim.rb: vim-config.git now under pivotalcommon

--- attributes/vim.rb
+++ attributes/vim.rb
@@ -1,3 +1,3 @@
 node.default["vim_home"]="#{WS_HOME}/.vim"
 node.default["vim_hash"]="c948a08572b6749b9c91aabada1f498e6e9d61a1"
-node.default["vim_config_git"]="git://github.com/pivotal/vim-config.git"
+node.default["vim_config_git"]="git://github.com/pivotalcommon/vim-config.git"
