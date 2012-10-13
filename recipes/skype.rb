358cd53121ea5d2686979db1aaff6b2b4acbdb03
Brian Cunnie <pair+cunnie@pivotallabs.com>
2012-10-12 18:04:50 -0700

    Fixed Skype recipe

--- recipes/skype.rb
+++ recipes/skype.rb
@@ -1,4 +1,4 @@
-pivotal_workstation_package app do
+pivotal_workstation_package Skype do
   source "http://download.skype.com/macosx/Skype_5.8.0.1027.dmg"
   checksum "64ec1beaca76f648e7a5ba3bb1084afab735cbe6d6b17f9e2f52e22e14fcc596"
   user WS_USER
