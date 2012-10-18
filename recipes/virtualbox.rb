30b3f9088fa2e3bb0aa0efbb36baad4d9f1c6e9f
Brian Cunnie <pair+cunnie@pivotallabs.com>
2012-10-17 21:30:27 -0700

    virtualbox: bumped to 4.2.1

--- recipes/virtualbox.rb
+++ recipes/virtualbox.rb
@@ -1,6 +1,6 @@
 pivotal_workstation_package "VirtualBox" do
-  source "http://download.virtualbox.org/virtualbox/4.1.22/VirtualBox-4.1.23-80870-OSX.dmg"
-  checksum "a73bdf9210868d3aeccbc6163445b2dd588a8ab591a8cf7005452673f7e6ba33"
+  source "http://download.virtualbox.org/virtualbox/4.2.0/VirtualBox-4.2.1-80871-OSX.dmg"
+  checksum "dda9cc85e4dfdc07b8c32e7286ac2963fcce2f2d42c24367992697aada1b6166"
   action :install
   type "pkg"
   package_id "org.virtualbox.pkg.virtualbox"
