cb5f0d862a1b289324474392146228573ebbaeb8
Brian Cunnie <pair+cunnie@pivotallabs.com>
2012-10-18 17:11:16 -0700

    java: bumped to 2012-006

--- recipes/java.rb
+++ recipes/java.rb
@@ -1,8 +1,8 @@
 pivotal_workstation_package "JavaForOSX" do
   source "http://support.apple.com/downloads/DL1572/en_US/"
-  volumes_dir "Java for OS X 2012-005"
+  volumes_dir "Java for OS X 2012-006"
   action :install
   type "pkg"
   package_id "com.apple.pkg.JavaForMacOSX107"
-  checksum "e0750c72972b8a2ccbcb3144bb31d74419276387"
+  checksum "6d947f4a20e8f7ee35bb460675ad0b930f8acc15ee0b0dbfc791a7a02abed6c0"
 end
