a7c85e7f6b5577c7ac3ae10c397ad38179850d4a
Brian Cunnie <pair+cunnie@pivotallabs.com>
2012-10-26 14:41:27 -0700

    ccmenu:  now starts up on login

--- recipes/ccmenu.rb
+++ recipes/ccmenu.rb
@@ -1,7 +1,18 @@
-pivotal_workstation_package "CCMenu" do
-  volumes_dir "CCMenu 1.3"
-  source "http://surfnet.dl.sourceforge.net/project/ccmenu/CCMenu/1.3/ccmenu-1.3-b.dmg"
-  checksum "9afef887022002b0f074eb7db25349da53aea5d5a64d8415b756c676db9ad1de"
-  owner WS_USER
-  action :install
+include_recipe "pivotal_workstation::addloginitem"
+
+app_path="/Applications/CCMenu.app"
+
+unless File.exists?(app_path)
+  pivotal_workstation_package "CCMenu" do
+    volumes_dir "CCMenu 1.3"
+    source "http://surfnet.dl.sourceforge.net/project/ccmenu/CCMenu/1.3/ccmenu-1.3-b.dmg"
+    checksum "9afef887022002b0f074eb7db25349da53aea5d5a64d8415b756c676db9ad1de"
+    owner WS_USER
+    action :install
+  end
+
+  execute "Start CCMenu on login" do
+    command "addloginitem #{app_path}"
+    user WS_USER
+  end
 end
