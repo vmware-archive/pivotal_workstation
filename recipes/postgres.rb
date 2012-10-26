2ca950211aeb732f59a6a0fa7b7bec9de551162f
Brian Cunnie <pair+cunnie@pivotallabs.com>
2012-10-26 16:05:19 -0700

    postgres: much-needed love
    Has 2 super-users: postgres & WS_USER
    
    Unloads the NEW homebrew plist as well as old one

--- recipes/postgres.rb
+++ recipes/postgres.rb
@@ -4,18 +4,20 @@ include_recipe "pivotal_workstation::user_owns_usr_local"
 
 run_unless_marker_file_exists("postgres") do
 
-  plist_path = File.expand_path('org.postgresql.postgres.plist', File.join('~', 'Library', 'LaunchAgents'))
-  if File.exists?(plist_path)
-    log "postgres plist found at #{plist_path}"
-    execute "unload the plist (shuts down the daemon)" do
-      command %'launchctl unload -w #{plist_path}'
-      user WS_USER
+  ["homebrew.mxcl.postgresql.plist", "org.postgresql.postgres.plist" ].each do |plist|
+    plist_path = File.expand_path(plist, File.join('~', 'Library', 'LaunchAgents'))
+    if File.exists?(plist_path)
+      log "postgres plist found at #{plist_path}"
+      execute "unload the plist (shuts down the daemon)" do
+        command %'launchctl unload -w #{plist_path}'
+        user WS_USER
+      end
+    else
+      log "Did not find plist at #{plist_path} don't try to unload it"
     end
-  else
-    log "Did not find plist at #{plist_path} don't try to unload it"
   end
 
-#    blow away default image's data directory
+# blow away default image's data directory
   directory "/usr/local/var/postgres" do
     action :delete
     recursive true
@@ -24,10 +26,10 @@ run_unless_marker_file_exists("postgres") do
   brew_install "postgresql"
 
   execute "create the database" do
-    command %'initdb -U postgres --encoding=utf8 --locale=en_US /usr/local/var/postgres'
+    command "initdb -U postgres --encoding=utf8 --locale=en_US /usr/local/var/postgres"
     user WS_USER
   end
-
+  
   launch_agents_path = File.expand_path('.', File.join('~','Library', 'LaunchAgents'))
   directory launch_agents_path do
     action :create
@@ -60,19 +62,11 @@ run_unless_marker_file_exists("postgres") do
   # Michael Sofaer says that these are probably the right settings:
   #   kern.sysv.shmall=65535
   #   kern.sysv.shmmax=16777216
-  #not_if File.exists?("/etc/sysctl.conf") do
-  # check if the sysctl variables are big enough
-  #   if not, set them
-  # check if /etc/sysctl.conf exists
-  #   if so, check if kern.sysv.{shmall,shmmax} are set
-  #     if so, check that they're set large enough
-  #       if so, go on
-  #   otherwise
-  #     modify /etc/sysctl.conf to make them big enough
-  # otherwise
-  #   create /etc/sysctl.conf & make these settings big enough
-  #
 
+  execute "create the postgres '#{WS_USER}' superuser" do
+    command "createuser -U postgres --superuser #{WS_USER}"
+    user WS_USER
+  end
 end
 
 ruby_block "test to see if postgres is running" do
