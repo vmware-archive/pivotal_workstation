c231352144b94d585a8b3d7a306a796b1d837632
Brian Cunnie <pair+cunnie@pivotallabs.com>
2012-10-26 13:13:43 -0700

    shiftit: now starts automatically

--- recipes/shiftit.rb
+++ recipes/shiftit.rb
@@ -1,18 +1,20 @@
+include_recipe "pivotal_workstation::addloginitem"
+
 unless File.exists?("/Applications/ShiftIt.app")
   remote_file "#{Chef::Config[:file_cache_path]}/ShiftIt.app.zip" do
     source "https://github.com/downloads/onsi/ShiftIt/ShiftIt.app.zip"
     mode "0644"
   end
 
-  # start up on login
-  execute "Start ShiftIt automatically" do
-    command "defaults write loginwindow AutoLaunchedApplicationDictionary -array-add '{ \"Path\" = \"/Applications/ShiftIt.app\"; \"Hide\" = 0; }'"
-    user WS_USER
-  end
-
   execute "unzip ShiftIt" do
     command "unzip #{Chef::Config[:file_cache_path]}/ShiftIt.app.zip ShiftIt.app/* -d /Applications/"
     user WS_USER
     group "admin"
+  end
+
+  # start up on login
+  execute "Start ShiftIt automatically" do
+    command "addloginitem /Applications/ShiftIt.app"
+    user WS_USER
   end
 end
