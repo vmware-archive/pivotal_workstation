a1ee4004ef440ec91a721bcfeadca65df642e67d
Brian Cunnie <pair+cunnie@pivotallabs.com>
2012-10-26 13:12:22 -0700

    addloginitem: now can add applications to start automatically

---
+++ recipes/addloginitem.rb
@@ -0,0 +1,19 @@
+include_recipe "pivotal_workstation::user_owns_usr_local"
+
+addloginitem_path="/usr/local/bin/addloginitem"
+
+unless File.exists?(addloginitem_path)
+
+  remote_file addloginitem_path do
+    source "https://github.com/downloads/pivotalexperimental/addLoginItem/addLoginItem"
+    mode '0755'
+    owner WS_USER
+  end
+
+  ruby_block "test to see if addloginitem was installed" do
+    block do
+      raise "#{addloginitem_path} was not installed" unless File.exists?(addloginitem_path)
+    end
+  end
+
+end
