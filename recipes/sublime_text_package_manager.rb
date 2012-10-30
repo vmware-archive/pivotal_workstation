d336727fbcdbe9a02f42c092acbe52af96251fb9
Matthew Robertson <matthewrobertson03@gmail.com>
2012-10-29 23:50:28 -0700

    removed unnecessary conditional in sublime_text_package_control recipe

--- recipes/sublime_text_package_manager.rb
+++ recipes/sublime_text_package_manager.rb
@@ -1,18 +1,14 @@
 package_dir = "#{WS_HOME}/Library/Application Support/Sublime Text 2/Installed Packages"
 filename    = "Package Control.sublime-package"
 
-unless File.exists?("#{package_dir}/#{filename}")
+execute "make sublimetext installed packages directory" do
+  command "mkdir -p '#{package_dir}'"
+  user WS_USER
+  not_if { File.exists?(package_dir) }
+end
 
-  execute "make sublimetext installed packages directory" do
-    command "mkdir -p '#{package_dir}'"
-    user WS_USER
-    not_if { File.exists?(package_dir) }
-  end
-
-  remote_file "#{package_dir}/#{filename}" do
-    source 'http://sublime.wbond.net/Package%20Control.sublime-package'
-    owner WS_USER
-    :create_if_missing
-  end
-
+remote_file "#{package_dir}/#{filename}" do
+  source 'http://sublime.wbond.net/Package%20Control.sublime-package'
+  owner WS_USER
+  :create_if_missing
 end
