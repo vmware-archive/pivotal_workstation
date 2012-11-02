0f4858c7d17849ed8b5b54d07ab9979c93063b82
Brian Cunnie <pair+cunnie@pivotallabs.com>
2012-11-02 13:52:27 -0700

    libreoffice: bumped to 3.6.3

--- recipes/libreoffice.rb
+++ recipes/libreoffice.rb
@@ -1,7 +1,7 @@
 pivotal_workstation_package "LibreOffice" do
   volumes_dir "LibreOffice"
-  source "http://download.documentfoundation.org/libreoffice/stable/3.6.2/mac/x86/LibO_3.6.2_MacOS_x86_install_en-US.dmg"
-  checksum "b0edd3dc065ba1e061a3073d7ee6125d7e7c12ff7dee06132fa9d792feb1ec72"
+  source "http://download.documentfoundation.org/libreoffice/stable/3.6.3/mac/x86/LibO_3.6.3_MacOS_x86_install_en-US.dmg"
+  checksum "d8537892049c053f91aff569a72c67bfd7559be4340588484025cf2d73adff5a"
   owner WS_USER
   action :install
 end
