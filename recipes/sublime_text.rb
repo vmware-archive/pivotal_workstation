7630bc9ebbf1869cafdd5fcf3a0edd426ad6d502
Brian Cunnie, Doc Ritezel & Matthew Kocher <pair+cunnie+doc+mkocher@pivotallabs.com>
2012-11-03 17:03:57 -0700

    sublime: merged package control

--- recipes/sublime_text.rb
+++ recipes/sublime_text.rb
@@ -1,8 +1,10 @@
+include_recipe "pivotal_workstation::user_owns_usr_local"
+
 dmg_package "Sublime Text 2" do
   dmg_name "Sublime%20Text%202.0.1"
   source "http://c758482.r82.cf2.rackcdn.com/Sublime%20Text%202.0.1.dmg"
   checksum "b5f91ee4f62d36c0490c021d5fb134b9e7cb3936"
-  owner WS_USER
+  # owner WS_USER
 end
 
 link "/usr/local/bin/subl" do
@@ -28,4 +30,17 @@ template File.expand_path("Preferences.sublime-settings", File.join(sublime_user
   source "sublime_text-Preferences.sublime-settings.erb"
   owner WS_USER
   action :create_if_missing
+end
+
+package_dir = "#{WS_HOME}/Library/Application Support/Sublime Text 2/Installed Packages"
+filename    = "Package Control.sublime-package"
+
+recursive_directories(["#{WS_HOME}/Library/Application Support", "Sublime Text 2", "Installed Packages"]) do
+  owner WS_USER
+end
+
+remote_file "#{package_dir}/#{filename}" do
+  source 'http://sublime.wbond.net/Package%20Control.sublime-package'
+  owner WS_USER
+  :create_if_missing
 end
