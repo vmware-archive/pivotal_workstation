88f99750e9be9fd4de1574a16c9dab2e713cbb8d
Roderik van der Veer <roderik.van.der.veer@kunstmaan.be>
2012-11-21 09:49:41 +0100

    Dropbox 1.6

--- recipes/dropbox.rb
+++ recipes/dropbox.rb
@@ -1,7 +1,6 @@
 dmg_package "Dropbox" do
   volumes_dir "Dropbox Installer"
-  source "http://dl-web.dropbox.com/u/17/b/Dropbox%201.4.12.dmg"
-  checksum "367f18673cc27ab9266d70765e5c33f9cfd59f91028d18bd1188de52c9422cf3"
-  # owner WS_USER
+  source "https://www.dropbox.com/download"
+  checksum "e80f8974b1759f85f98c43c5ba8784f400ff9da507cf73077c2366c15bb04e33"
   action :install
 end
