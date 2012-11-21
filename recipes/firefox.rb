758913f0a5fdc20fb6970e33a51de80ec173316b
Roderik van der Veer <roderik.van.der.veer@kunstmaan.be>
2012-11-21 11:31:28 +0100

    Update to firefox 17

--- recipes/firefox.rb
+++ recipes/firefox.rb
@@ -1,6 +1,5 @@
 dmg_package "Firefox" do
-  source "http://download.mozilla.org/?product=firefox-16.0.1&os=osx&lang=en-US"
-  checksum "5911843c3b4c2a55105ceacba8a377bce897c984ee202bed963df34089fecae9"
-  # owner WS_USER
+  source "http://download.mozilla.org/?product=firefox-17.0&os=osx&lang=en-US"
+  checksum "abd7e8a4da8faf815d0bd96a6b1a6492e630c73433522c0c3dfd671f46cc2236"
   action :install
 end
