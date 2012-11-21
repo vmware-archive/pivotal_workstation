361a6f58185053a0a8a8989c47da34d6f4d89088
Roderik van der Veer <roderik.van.der.veer@kunstmaan.be>
2012-11-21 09:57:39 +0100

    Force platform

--- recipes/dropbox.rb
+++ recipes/dropbox.rb
@@ -1,6 +1,6 @@
 dmg_package "Dropbox" do
   volumes_dir "Dropbox Installer"
-  source "https://www.dropbox.com/download"
+  source "https://www.dropbox.com/download?plat=mac"
   checksum "e80f8974b1759f85f98c43c5ba8784f400ff9da507cf73077c2366c15bb04e33"
   action :install
 end
