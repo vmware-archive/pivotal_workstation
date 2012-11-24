0f1eb101b2b1e4dbc2c94d52de100120e95682b5
Matthew Rothenberg <mrothenberg@gmail.com>
2012-11-23 19:11:34 -0500

    update rvm to 1.17.0 (stable)

--- attributes/versions.rb
+++ attributes/versions.rb
@@ -1,5 +1,5 @@
 node.default['versions']= {
   "homebrew" => "afa5150fabd2fb5247e32bcef7ed561635191143",
   "bash_it" => "5cb0ecc1c813bc5619e0f708b8015a4596a37d6c",
-  "rvm" => "85e0d3e8d1e63e7620335c38c755a63a4c134487"
+  "rvm" => "213d4ded2bda955ed3b3a87c9dd0823e1e516dfb"
 }
