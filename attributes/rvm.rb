3e6f51babded3173b8c844ad0936d61f3a6484e6
Brian Cunnie <pair+cunnie@pivotallabs.com>
2012-11-26 12:49:29 -0800

    Rolled back to ruby-1.9.3-p194
    1.9.3-p327 failed:
    in .rvm/log/ruby-1.9.3-p327/make.log
    readline.c:1499:9: error: use of undeclared identifier
    'username_completion_function'; did you mean
    'rl_username_completion_function'?

--- attributes/rvm.rb
+++ attributes/rvm.rb
@@ -5,8 +5,7 @@ node.default["rvm"]= {
     #"ree-1.8.7-2012.02" => { :env => "CC=gcc-4.2" },
     #"ruby-1.8.7-p358" => { :env => "CC=gcc-4.2" },
     #"ruby-1.9.2-p290" => { :env => "CC=gcc-4.2" },
-    #"ruby-1.9.3-p194" => { :command_line_options => "--with-gcc=clang" },
-    "ruby-1.9.3-p327" => { :command_line_options => "--with-gcc=clang" },
+    "ruby-1.9.3-p194" => { :command_line_options => "--with-gcc=clang" },
   },
-  "default_ruby" => "ruby-1.9.3-p327"
+  "default_ruby" => "ruby-1.9.3-p194"
 }
