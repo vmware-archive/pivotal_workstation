0966148f000d6847e095aa4fc6dbdc73aa5ff763
Doc Ritezel <pair+doc@pivotallabs.com>
2012-12-01 18:26:11 -0800

    stop requiring bash_it in bash_it_custom_plugin

--- providers/bash_it_custom_plugin.rb
+++ providers/bash_it_custom_plugin.rb
@@ -1,6 +1,4 @@
 action :create do
-  include_recipe "pivotal_workstation::bash_it"
-
   bash_it_custom_dir = ::File.join(::BASH_IT_DIR, "custom")
 
   template ::File.expand_path(custom_script, bash_it_custom_dir) do
