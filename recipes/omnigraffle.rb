72decbc40b2bfd6a59de4b5f277a88695705f317
Roderik van der Veer <roderik@vanderveer.be>
2012-12-16 12:39:34 +0100

    Omnigraffle Professional

---
+++ recipes/omnigraffle.rb
@@ -0,0 +1,7 @@
+dmg_package "OmniGraffle Professional 5" do
+  volumes_dir "OmniGrafflePro"
+  accept_eula true
+  source "http://www.omnigroup.com/download/latest/omnigrafflepro"
+  action :install
+  owner WS_USER
+end
