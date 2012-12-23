b18649148bb3d7c86580d15e540cb6a6b285008e
Doc Ritezel <pair+doc@pivotallabs.com>
2012-12-23 00:54:50 -0800

    Clean up Github for Mac recipe

--- attributes/github_for_mac.rb
+++ attributes/github_for_mac.rb
@@ -1,4 +1,3 @@
-# node.default["github_for_mac_download_uri"]="https://central.github.com/mac/latest"
-# too bad latest is redirect to url with spaces in the name :(
-node.default["github_for_mac_download_uri"]="https://github-central.s3.amazonaws.com/mac%2FGitHub%20for%20Mac%2087.zip"
+node.default["github_for_mac_download_uri"]="https://github-central.s3.amazonaws.com/mac%2FGitHub%20for%20Mac%2096.zip"
+node.default["github_for_mac_sha"]="0411b4e57b795fb4e12b8d14751a95ec49db4c5bc1d67f0f842875059350d710"
 node.default["github_for_mac_app_path"]="/Applications/GitHub.app"
