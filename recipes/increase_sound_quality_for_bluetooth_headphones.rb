9ad1996c4fa5351ec0654bbe3a76648fe5f80c77
Roderik van der Veer <roderik@vanderveer.be>
2012-12-31 12:07:33 +0100

    Increase sound quality for Bluetooth headphones/headsets

---
+++ recipes/increase_sound_quality_for_bluetooth_headphones.rb
@@ -0,0 +1,5 @@
+pivotal_workstation_defaults "Increase sound quality for Bluetooth headphones/headsets" do
+  domain 'com.apple.BluetoothAudioAgent'
+  key 'Apple Bitpool Min (editable)'
+  integer 40
+end
