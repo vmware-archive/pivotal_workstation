3025611b6682fef4a0c5330998e46963bb84207d
Roderik van der Veer <roderik@vanderveer.be>
2013-01-03 20:59:11 +0100

    Re-enable version limitation

--- recipes/bash_it.rb
+++ recipes/bash_it.rb
@@ -2,7 +2,7 @@ bash_it_version = version_string_for('bash_it')
 
 git "#{Chef::Config[:file_cache_path]}/bash_it" do
   repository node['bash_it']['repository']
-#  revision bash_it_version
+  revision bash_it_version
   destination "#{Chef::Config[:file_cache_path]}/bash_it"
   action :sync
 end
