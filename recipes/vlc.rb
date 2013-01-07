732e38906f0ee5e352cc65e48dbd8ea4754c0e5d
Cliff Erson <cerson@me.com>
2013-01-07 13:55:50 -0800

    Add vlc version and checksum sha to attribute
    This should be something that doesnt have to update very often as VLC
    uses sparkle framework for auto updates.

--- recipes/vlc.rb
+++ recipes/vlc.rb
@@ -1,10 +1,11 @@
-vlc_version = '2.0.5'
+vlc_version = node["vlc_version"]
+vlc_checksum = node["vlc_checksum"]
 
 dmg_package "VLC" do
   dmg_name "vlc-#{vlc_version}"
   volumes_dir "vlc-#{vlc_version}"
-  source "http://download.videolan.org/vlc/2.0.5/macosx/vlc-#{vlc_version}.dmg"
-  checksum "64257a95902314248128e52b2d90e13e0475e57ad3661000ab2226c23a7395bf"
+  source "http://download.videolan.org/vlc/#{vlc_version}/macosx/vlc-#{vlc_version}.dmg"
+  checksum vlc_checksum
   action :install
   owner WS_USER
 end
