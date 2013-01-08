52e59fefc2a587cac962dfdee9dd4d08a90bfb6b
Jérôme Gamez <jerome.gamez@gmail.com>
2013-01-08 15:20:04 +0100

    Add recipe for Adium

---
+++ recipes/adium.rb
@@ -0,0 +1,7 @@
+dmg_package "Adium" do
+  volumes_dir "Adium 1.5.4"
+  source "http://sourceforge.net/projects/adium/files/Adium_1.5.4.dmg/download"
+  checksum "c11302c03c90bffeafb25b36a0a77c97fc27f32f2c32845df2292416b46457aa"
+  action :install
+  owner WS_USER
+end
