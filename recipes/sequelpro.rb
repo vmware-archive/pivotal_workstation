a643b5b060032b061cb4f2dbeb4f71647e893e08
Brian Cunnie and Doc Ritezel <pair+cunnie+doc@pivotallabs.com>
2013-01-18 17:18:51 -0800

    Added Sequel Pro checksum

--- recipes/sequelpro.rb
+++ recipes/sequelpro.rb
@@ -2,5 +2,6 @@ dmg_package "Sequel Pro" do
   volumes_dir "Sequel Pro 1.0"
   owner WS_USER
   source "http://sequel-pro.googlecode.com/files/sequel-pro-1.0-RC1.dmg"
+  checksum "1cf4824897782239c0283d8cacd838121b94abd1f4e0ac386ca5839ed32b81b9"
   action :install
 end
