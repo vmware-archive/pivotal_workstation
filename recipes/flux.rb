d93a1f86744d746b2c289138d77191c8f89d0ec0
Brian Cunnie and Doc Ritezel <pair+cunnie+doc@pivotallabs.com>
2013-01-18 17:02:33 -0800

    refactored flux; quieter, better not_if

--- recipes/flux.rb
+++ recipes/flux.rb
@@ -1,14 +1,13 @@
-unless File.exists?("/Applications/Flux.app")
+remote_file "#{Chef::Config[:file_cache_path]}/Flux.zip" do
+  source node["flux_download_uri"]
+  checksum node["flux_download_checksum"]
+  mode "0644"
+  not_if { File.exists?("/Applications/Flux.app") }
+end
 
-  remote_file "#{Chef::Config[:file_cache_path]}/Flux.zip" do
-    source node["flux_download_uri"]
-    checksum node["flux_download_checksum"]
-    mode "0644"
-  end
-
-  execute "unzip flux" do
-    command "unzip #{Chef::Config[:file_cache_path]}/Flux.zip Flux.app/* -d /Applications/"
-    user WS_USER
-    group "admin"
-  end
+execute "unzip flux" do
+  command "unzip -q #{Chef::Config[:file_cache_path]}/Flux.zip Flux.app/* -d /Applications/"
+  user WS_USER
+  group "admin"
+  not_if { File.exists?("/Applications/Flux.app") }
 end
