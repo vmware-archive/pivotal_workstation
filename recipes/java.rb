0a573b450e3565b401b8f828550af6f87deb1ddd
Patrick Wyatt <patrickjohnwyatt@gmail.com>
2013-01-24 23:22:58 -0800

    Working on my system

--- recipes/java.rb
+++ recipes/java.rb
@@ -1,3 +1,6 @@
+# No longer supported/needed for OSX >= 10.7.5
+
+=begin
 dmg_package "JavaForOSX" do
   source "http://support.apple.com/downloads/DL1572/en_US/"
   volumes_dir "Java for OS X 2012-006"
@@ -6,3 +9,4 @@ dmg_package "JavaForOSX" do
   package_id "com.apple.pkg.JavaForMacOSX107"
   checksum "6d947f4a20e8f7ee35bb460675ad0b930f8acc15ee0b0dbfc791a7a02abed6c0"
 end
+=end
