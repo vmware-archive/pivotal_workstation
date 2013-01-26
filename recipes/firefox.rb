46349e69c91dd42cb08edf6d5790e59f1ad4904a
Patrick Wyatt <pat@codeofhonor.com>
2013-01-26 08:39:59 +0000

    Added Firefox for Linux

--- recipes/firefox.rb
+++ recipes/firefox.rb
@@ -1,6 +1,12 @@
-dmg_package "Firefox" do
-  source "http://download.mozilla.org/?product=firefox-17.0&os=osx&lang=en-US"
-  checksum "abd7e8a4da8faf815d0bd96a6b1a6492e630c73433522c0c3dfd671f46cc2236"
-  action :install
-  owner WS_USER
+if node["platform"] == "mac_os_x"
+  dmg_package "Firefox" do
+    source "http://download.mozilla.org/?product=firefox-17.0&os=osx&lang=en-US"
+    checksum "abd7e8a4da8faf815d0bd96a6b1a6492e630c73433522c0c3dfd671f46cc2236"
+    action :install
+    owner WS_USER
+  end
+else
+  # CentOS: this depends upon the "remi" repo (or similar)
+  # already being pre-configured for yum.
+  package "firefox"
 end
