b895d2aadf15f3f3a0574d0513c2fd7c0a904331
Matt Heath <matt@mattheath.com>
2013-02-04 22:35:47 +0000

    Tidy up config key paths

--- recipes/charles_proxy.rb
+++ recipes/charles_proxy.rb
@@ -13,7 +13,7 @@
 remote_file "#{Chef::Config[:file_cache_path]}/charles_proxy.zip" do
   source node["charles_proxy"]["download_uri"]
   owner WS_USER
-  checksum node["charles_proxy_sha"]
+  checksum node["charles_proxy"]["sha"]
   not_if { File.exists?("#{Chef::Config[:file_cache_path]}/charles_proxy.zip") }
 end
 
@@ -24,22 +24,22 @@ execute "unzip charles_proxy" do
 end
 
 execute "copy charles_proxy to /Applications" do
-  command "mv #{Chef::Config[:file_cache_path]}/Charles.app #{Regexp.escape(node["charles_proxy_app_path"])}"
+  command "mv #{Chef::Config[:file_cache_path]}/Charles.app #{Regexp.escape(node["charles_proxy"]["app_path"])}"
   user WS_USER
   group "admin"
-  not_if { File.exists?(node["charles_proxy_app_path"]) }
+  not_if { File.exists?(node["charles_proxy"]["app_path"]) }
 end
 
-template node["charles_proxy_config_path"] do
+template node["charles_proxy"]["config_path"] do
   source "com.xk72.charles.config.erb"
   cookbook 'pivotal_workstation'
   owner WS_USER
   mode "0777"
-  not_if { File.exists?(node["charles_proxy_config_path"]) || node["charles_proxy"]["license_key"] == nil || node["charles_proxy"]["license_name"] == nil }
+  not_if { File.exists?(node["charles_proxy"]["config_path"]) || node["charles_proxy"]["license_key"] == nil || node["charles_proxy"]["license_name"] == nil }
 end
 
 ruby_block "test to see if Charles.app was installed" do
   block do
-    raise "Charles.app was not installed" unless File.exists?(node["charles_proxy_app_path"])
+    raise "Charles.app was not installed" unless File.exists?(node["charles_proxy"]["app_path"])
   end
 end
