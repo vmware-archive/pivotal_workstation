be4ca3de661582bf0e484740a1708870ee232e89
Steve Marshall <steve@nascentguruism.com>
2013-02-06 22:13:06 +0000

    Make installation of Things overridable.

--- recipes/things.rb
+++ recipes/things.rb
@@ -1,7 +1,7 @@
-unless File.exists?("/Applications/Things.app")
+unless File.exists?("#{node["things"]["destination"]}/Things.app")
 
   remote_file "#{Chef::Config[:file_cache_path]}/Things.zip" do
-    source "http://culturedcode.com/things/download/"
+    source node["things"]["source"]
     owner WS_USER
   end
 
@@ -10,15 +10,15 @@ unless File.exists?("/Applications/Things.app")
     user WS_USER
   end
 
-  execute "copy Things to /Applications" do
-    command "mv #{Chef::Config[:file_cache_path]}/Things.app #{Regexp.escape("/Applications/Things.app")}"
+  execute "copy Things to #{node["things"]["destination"]}" do
+    command "mv #{Chef::Config[:file_cache_path]}/Things.app #{Regexp.escape("#{node["things"]["destination"]}/Things.app")}"
     user WS_USER
     group "admin"
   end
 
   ruby_block "test to see if Things.app was installed" do
     block do
-      raise "Things.app was not installed" unless File.exists?("/Applications/Things.app")
+      raise "Things.app was not installed" unless File.exists?("#{node["things"]["destination"]}/Things.app")
     end
   end
 
