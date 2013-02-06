dc37d49b41f18f3777da377bd889d02e64abb981
Steve Marshall <steve@nascentguruism.com>
2013-02-06 22:02:09 +0000

    Make installation of textmate overridable.

--- recipes/textmate.rb
+++ recipes/textmate.rb
@@ -2,8 +2,10 @@ include_recipe "pivotal_workstation::user_owns_usr_local"
 
 node.default["textmate"]["url"] = "http://download.macromates.com/TextMate_1.5.10.zip"
 node.default["textmate"]["shasum"] = "325f061fb19f87ea61df672df619065ea34e2c88fba30c84635368ea0a40c406"
+node.default["textmate"]["destination"] = "/Applications"
+node.default["textmate"]["cmd_destination"] = "/usr/local/bin"
 
-unless File.exists?("/Applications/TextMate.app")
+unless File.exists?("#{node["textmate"]["destination"]}/TextMate.app")
   directory Chef::Config[:file_cache_path] do
     action :create
     recursive true
@@ -15,8 +17,8 @@ unless File.exists?("/Applications/TextMate.app")
     owner WS_USER
   end
 
-  execute "extract text mate to /Applications" do
-    command "unzip -o #{Chef::Config[:file_cache_path]}/textmate.zip -x __MACOSX* -d /Applications/"
+  execute "extract text mate to #{Regexp.escape(node["textmate"]["destination"])}" do
+    command "unzip -o #{Chef::Config[:file_cache_path]}/textmate.zip -x __MACOSX* -d #{Regexp.escape(node["textmate"]["destination"])}"
     user WS_USER
 
     # This is required to unzip into Applications
@@ -24,18 +26,18 @@ unless File.exists?("/Applications/TextMate.app")
   end
 
   execute "link textmate" do
-    command "ln -s /Applications/TextMate.app/Contents/Resources/mate /usr/local/bin/mate"
-    not_if "test -e /usr/local/bin/mate"
+    command "ln -s #{Regexp.escape(node["textmate"]["destination"])}/TextMate.app/Contents/Resources/mate #{Regexp.escape(node["textmate"]["cmd_destination"])}/mate"
+    not_if "test -e #{Regexp.escape(node["textmate"]["cmd_destination"])}/mate"
   end
 
   ruby_block "test to see if TextMate was installed" do
     block do
-      raise "TextMate install failed" unless File.exists?("/Applications/TextMate.app")
+      raise "TextMate install failed" unless File.exists?("#{Regexp.escape(node["textmate"]["destination"])}/TextMate.app")
     end
   end
 end
 
 execute "link textmate" do
-  command "ln -s /Applications/TextMate.app/Contents/Resources/mate /usr/local/bin/mate"
-  not_if "test -e /usr/local/bin/mate"
+  command "ln -s #{Regexp.escape(node["textmate"]["destination"])}/TextMate.app/Contents/Resources/mate #{Regexp.escape(node["textmate"]["cmd_destination"])}/mate"
+  not_if "test -e #{Regexp.escape(node["textmate"]["cmd_destination"])}/mate"
 end
