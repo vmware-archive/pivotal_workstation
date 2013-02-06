41e0fada37484250321a619d8812db83e19b20ba
Steve Marshall <steve@nascentguruism.com>
2013-02-06 22:01:57 +0000

    Make installation of Dropbox overridable.

--- recipes/dropbox.rb
+++ recipes/dropbox.rb
@@ -1,7 +1,8 @@
 dmg_package "Dropbox" do
   volumes_dir "Dropbox Installer"
-  source "https://www.dropbox.com/download?plat=mac"
-  checksum "e80f8974b1759f85f98c43c5ba8784f400ff9da507cf73077c2366c15bb04e33"
+  source node["dropbox"]["source"]
+  checksum node["dropbox"]["checksum"]
   action :install
   owner WS_USER
+  destination node["dropbox"]["destination"]
 end
