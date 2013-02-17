892545934c6b62c04a58cbd5ea399c1ffa5117c1
Abhijit Hiremagalur <abhijit@hiremaga.com>
2013-02-17 14:56:25 -0800

    Upgrade RVM to 1.18.8
    * Fixes Rubygems SSL issue #221

--- attributes/versions.rb
+++ attributes/versions.rb
@@ -1,5 +1,5 @@
 node.default['versions']= {
   "homebrew" => "afa5150fabd2fb5247e32bcef7ed561635191143",
   "bash_it" => "5cb0ecc1c813bc5619e0f708b8015a4596a37d6c",
-  "rvm" => "213d4ded2bda955ed3b3a87c9dd0823e1e516dfb"
+  "rvm" => "88ca5e086e01e9e29bdeb494cb6a3ccda4c7e256"
 }
