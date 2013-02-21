542588219c35a756ae50772ecf3e978426c0ca75
Jacob Maine <jacob.maine@gmail.com>
2013-02-20 22:42:42 -0500

    Simplify vim install; remove RVM dependency
    This commit does a number of things to simplify and improve the vim
    recipe:
    
    - Re-enable Command-T.
    
    - Remove dependency on RVM.  Both MacVim, and Command-T must be compiled
      with the system ruby.  Originally we used RVM to access the system
      ruby, but not everyone wants to use RVM.  The latest versions of
      homebrew use the system ruby automatically when installing MacVim, so
      that part is already fixed.  This commit compiles Command-T with the
      system ruby too, using similar tactics to homebrew.
    
    - MacVim also installs a command line vim.  Use that one everywhere,
      instead of installing a separate command line vim.  This does NOT mean
      you have to use MacVim or that the command line doesn't work, only
      that they both rely on the same underlying install of vim.
    
    - Remove dependency on mercurial, because we don't need to download vim
      from a mercurial repo.
    
    - Stop blowing away ~/.vim on every run of soloist.  This was causing
      soloist to re-download every vim plugin on every run.  If you really
      want to force soloist to re-install vim, do something like the
      following:
        brew uninstall macvim
        rm -rf ~/.vim*
        rm -rf /Applications/MacVim.app
    
    This commit builds on Doc Ritezel's initial work to modernize the vim
    recipe.

--- recipes/vim.rb
+++ recipes/vim.rb
@@ -4,38 +4,25 @@
 include_recipe "pivotal_workstation::homebrew"
 include_recipe "pivotal_workstation::ack"
 include_recipe "pivotal_workstation::git"
-include_recipe "pivotal_workstation::rvm"
 include_recipe "pivotal_workstation::tmux"
 
-brew_install "mercurial"
-
-execute "install vim from url" do
-  command "brew install #{node["vim_homebrew_url"]}"
-  user WS_USER
-  not_if "brew list | grep '^vim$'"
-end
-
-execute "install macvim with system ruby" do
-  command "rvm use system; brew install macvim"
+execute "install macvim and use its vim as system vim" do
+  command "brew install macvim --override-system-vim"
   user WS_USER
   not_if "brew list | grep '^macvim$'"
 end
 
 execute "link macvim into place" do
+  # Not using `brew linkapps` because that links to ~/Applications, not
+  # /Applications.  If you decide linkapps is better, be sure to modify the
+  # "verify that command-t is correctly compiled" task.
   command "ln -s /usr/local/Cellar/macvim/*/MacVim.app /Applications/MacVim.app"
   user WS_USER
   not_if { File.symlink?("/Applications/MacVim.app") }
 end
 
 execute "test to see if macvim link worked" do
-  command "test -e /Applications/MacVim.app"
-end
-
-directory "remove pre-existing vim configuration" do
-  path node["vim_home"]
-  action :delete
-  recursive true
-  only_if { File.directory?(node["vim_home"]) }
+  command "test -L /Applications/MacVim.app"
 end
 
 git node["vim_home"] do
@@ -47,10 +34,6 @@ git node["vim_home"] do
   enable_submodules true
 end
 
-execute "verify checkout happened" do
-  command "test -e #{node["vim_home"]}"
-end
-
 %w{vimrc gvimrc}.each do |vimrc|
   link "#{WS_HOME}/.#{vimrc}" do
     to "#{node["vim_home"]}/#{vimrc}"
@@ -59,20 +42,23 @@ end
   end
 end
 
-#execute "compile command-t" do
-#  command "rvm use system; ruby extconf.rb && make clean && make"
-#  cwd "#{node["vim_home"]}/bundle/command-t/ruby/command-t"
-#  user WS_USER
-#  only_if "test -d #{node["vim_home"]}/bundle/command-t/ruby/command-t"
-#end
+execute "compile command-t" do
+  # This matches the ruby that homebrew currently uses to `brew install macvim`
+  # See --with-ruby-command in https://github.com/mxcl/homebrew/blob/master/Library/Formula/macvim.rb
+  command "/System/Library/Frameworks/Ruby.framework/Versions/1.8/usr/bin/ruby extconf.rb && make clean && make"
+  cwd "#{node["vim_home"]}/bundle/command-t/ruby/command-t"
+  user WS_USER
+  only_if "test -d #{node["vim_home"]}/bundle/command-t/ruby/command-t"
+end
 
-#execute "verify that command-t is correctly compiled" do
-#  command %{test "`otool -l #{node["vim_home"]}/bundle/command-t/ruby/command-t/ext.bundle | grep libruby`" = "`otool -l /usr/local/bin/vim | grep libruby`"}
-#end
-
-#execute "verify that command-t is correctly compiled for macvim" do
-#  command %{test "`otool -l #{node["vim_home"]}/bundle/command-t/ruby/command-t/ext.bundle | grep libruby`" = "`otool -l /Applications/MacVim.app/Contents/MacOS/Vim | grep libruby`"}
-#end
+execute "verify that command-t is correctly compiled for macvim" do
+  command <<-SH
+    findlibruby () { otool -l $1 | grep libruby; }
+    CMDT_RUBY=`findlibruby #{node["vim_home"]}/bundle/command-t/ruby/command-t/ext.bundle`
+    MACVIM_RUBY=`findlibruby /Applications/MacVim.app/Contents/MacOS/Vim`
+    test -n "$MACVIM_RUBY" -a "$MACVIM_RUBY" = "$CMDT_RUBY"
+  SH
+end
 
 file "#{WS_HOME}/.vimrc.local" do
   action :touch
