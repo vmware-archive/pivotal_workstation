aba7b207048d978e20131c91b0b3c5ea71c4beab
Brian Cunnie and Colin Deeb <pair+cunnie+deeb@pivotallabs.com>
2013-02-22 17:24:27 -0800

    Remove java from default install

--- recipes/rubymine.rb
+++ recipes/rubymine.rb
@@ -1,5 +1,3 @@
-include_recipe "pivotal_workstation::java"
-
 dmg_package "RubyMine" do
   source "http://download.jetbrains.com/ruby/RubyMine-4.5.4.dmg"
   checksum "1983d4417c6b2817bd6534f8804c2366fbd074a17dbc5a98cd85161abe255714"
