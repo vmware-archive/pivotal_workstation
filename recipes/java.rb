99ce82c67f4f0ffe3df1ad2a7c344e31cab64dcc
Abhi Hiremagalur <abhijit@hiremaga.com>
2013-02-23 10:42:40 -0800

    Update checksum to match new Java dmg

--- recipes/java.rb
+++ recipes/java.rb
@@ -6,5 +6,5 @@ dmg_package "JavaForOSX" do
   action :install
   type "pkg"
   package_id "com.apple.pkg.JavaForMacOSX107"
-  checksum "6d947f4a20e8f7ee35bb460675ad0b930f8acc15ee0b0dbfc791a7a02abed6c0"
+  checksum "5c6f709b747a9c067580accf260a18cadba13ad16203adf8238929b5b2869f8b"
 end
