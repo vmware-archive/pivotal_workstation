f6b6891545801159a3cd0c9979a654769c8ce187
Abhi Hiremagalur <abhijit@hiremaga.com>
2013-03-02 12:40:54 -0800

    Upgrade to rvm 1.18.15
    * Adds ruby-1.9.3-p392 & ruby-2.0.0-p0

--- attributes/versions.rb
+++ attributes/versions.rb
@@ -1,5 +1,5 @@
 node.default['versions']= {
   "homebrew" => "afa5150fabd2fb5247e32bcef7ed561635191143",
   "bash_it" => "5cb0ecc1c813bc5619e0f708b8015a4596a37d6c",
-  "rvm" => "88ca5e086e01e9e29bdeb494cb6a3ccda4c7e256"
+  "rvm" => "d2c2e4d453f7b9723e63ebee3b0ea037f0c6bbad"
 }
