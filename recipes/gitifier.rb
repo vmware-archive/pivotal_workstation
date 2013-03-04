fca33c8add071986047531b3205de1d681629ac6
Arvind Kunday <hi@kunday.com>
2013-03-04 19:58:00 +1100

    Updating gitifier download location.

--- recipes/gitifier.rb
+++ recipes/gitifier.rb
@@ -1,6 +1,6 @@
 unless File.exists?("/Applications/Gitifier.app")
   remote_file "#{Chef::Config[:file_cache_path]}/Gitifier.zip" do
-    source "http://github.com/downloads/psionides/Gitifier/Gitifier-1.3.zip"
+    source "http://github.com/downloads/jsuder/Gitifier/Gitifier-1.3.zip"
     checksum "b199743bdae2305db99d9d3a247f9c8846031d1fbb19201ea81262b25e629d2f"
     owner WS_USER
   end
