c8e0e94c4fe72510de5b985b76ea7a45eda3c014
Abhi Hiremagalur and Brian Cunnie <pair+abhi+cunnie@pivotallabs.com>
2013-03-04 19:02:14 -0800

    Use master for homebrew
    Homebrew wants to live on the edge. This said it's still possible to
    customize your version to a sha if you'd prefer to lock things down for
    your project.

--- attributes/versions.rb
+++ attributes/versions.rb
@@ -1,5 +1,5 @@
 node.default['versions']= {
-  "homebrew" => "57ae9c7ace04fb1b54bd1d863fd3f701afc62f46",
+  "homebrew" => "master",
   "bash_it" => "5cb0ecc1c813bc5619e0f708b8015a4596a37d6c",
   "rvm" => "d2c2e4d453f7b9723e63ebee3b0ea037f0c6bbad"
 }
