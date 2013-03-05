0c4349e86c5c14defc7e5f88655581264118d5bd
Sam Bao <sam.bao@cerner.com>
2013-03-04 23:19:32 -0600

    Add Firefox Aurora recipe

---
+++ recipes/firefox_aurora.rb
@@ -0,0 +1,11 @@
+# coding: UTF-8
+
+#Installs Firefox Aurora (Pre Beta Build of Firefox)
+#http://www.mozilla.org/en-US/firefox/aurora/
+dmg_package "Firefox Aurora" do
+  dmg_name "firefox-21.0a2.en-US.mac.dmg"
+  source "https://ftp.mozilla.org/pub/mozilla.org/firefox/nightly/latest-mozilla-aurora/firefox-21.0a2.en-US.mac.dmg"
+  checksum "6afdb582f5c618b4dea4d0fba51aefa99c63d5d509103c89e0fc1ab072853387"
+  action :install
+  owner WS_USER
+end
