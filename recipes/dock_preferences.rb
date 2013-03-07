911ef454e2772bd369dc1ccc801e7d8eb80cfde1
Scott Vesely <svesely@gmail.com>
2013-03-07 11:58:18 -0800

    Adding attributes for dock preference override

--- recipes/dock_preferences.rb
+++ recipes/dock_preferences.rb
@@ -1,13 +1,13 @@
 pivotal_workstation_defaults "set dock to be on left" do
   domain "com.apple.dock"
   key "orientation"
-  string "left"
+  string node["dock_preferences"]["orientation"]
 end
 
 pivotal_workstation_defaults "set dock to autohide" do
   domain "com.apple.dock"
   key "autohide"
-  boolean true
+  boolean node["dock_preferences"]["autohide"]
 end
 
 execute "relaunch dock" do
