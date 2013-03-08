23cef90a845950f32de3fa5cc449be06cc387e08
Brian Cunnie <pair+cunnie@pivotallabs.com>
2013-03-08 15:01:47 -0800

    Java now at 2013-002

--- recipes/java.rb
+++ recipes/java.rb
@@ -2,9 +2,9 @@ return unless node["platform"] == "mac_os_x"
 
 dmg_package "JavaForOSX" do
   source "http://support.apple.com/downloads/DL1572/en_US/"
-  volumes_dir "Java for OS X 2013-001"
+  volumes_dir "Java for OS X 2013-002"
   action :install
   type "pkg"
   package_id "com.apple.pkg.JavaForMacOSX107"
-  checksum "5c6f709b747a9c067580accf260a18cadba13ad16203adf8238929b5b2869f8b"
+  checksum "0ac0ab9bd1990add28bb1fa088fb8e59b657a09cbf5fde1c9d38eea8459760cc"
 end
