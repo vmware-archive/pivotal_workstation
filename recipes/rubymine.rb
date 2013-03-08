edcac5ec7325ae93e552d9b0e2112509d0329c4c
Brian Cunnie <pair+cunnie@pivotallabs.com>
2013-03-08 15:14:12 -0800

    RubyMine requires java

--- recipes/rubymine.rb
+++ recipes/rubymine.rb
@@ -1,3 +1,5 @@
+include_recipe "pivotal_workstation::java"
+
 dmg_package "RubyMine" do
   source "http://download.jetbrains.com/ruby/RubyMine-5.0.2.dmg"
   checksum "4c186bbd54e976d2a83595bcc77a57304159ba7e40d6fe2e5690cb074b4c65ae"
