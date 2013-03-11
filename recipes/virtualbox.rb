4f4cd7db15ecdeac6d78e0c3363bf13f8c421362
Sam Bao <sam.bao@cerner.com>
2013-03-11 15:56:00 -0500

    update virtualbox to 4.2.8

--- recipes/virtualbox.rb
+++ recipes/virtualbox.rb
@@ -1,6 +1,6 @@
 dmg_package "VirtualBox" do
-  source "http://download.virtualbox.org/virtualbox/4.2.4/VirtualBox-4.2.4-81684-OSX.dmg"
-  checksum "6908705667dda65932005318ee16bff5ca0ba9dfdc89e9e7bf86a9f7469bcbf5"
+  source "http://download.virtualbox.org/virtualbox/4.2.8/VirtualBox-4.2.8-83876-OSX.dmg"
+  checksum "e090da9766a1780a4f1fc6b3b85fe8b7ce71c8030d1fda9e7178716009d69a33"
   action :install
   owner WS_USER
   type "pkg"
