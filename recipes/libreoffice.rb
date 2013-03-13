7a01268eb0153b541305950b958df2be9c17bd55
Brian Cunnie <pair+cunnie@pivotallabs.com>
2013-03-12 19:17:07 -0700

    Libre Office 4.0.1

--- recipes/libreoffice.rb
+++ recipes/libreoffice.rb
@@ -1,7 +1,7 @@
 dmg_package "LibreOffice" do
   volumes_dir "LibreOffice"
-  source "http://download.documentfoundation.org/libreoffice/stable/4.0.0/mac/x86/LibreOffice_4.0.0_MacOS_x86.dmg"
-  checksum "2ef287af5a94561fadf0d91a8a11ce936f97f59d360f1c6caa275fd601735c2f"
+  source "http://download.documentfoundation.org/libreoffice/stable/4.0.1/mac/x86/LibreOffice_4.0.1_MacOS_x86.dmg"
+  checksum "b34026a4b63e9f676364f5cfc5e8f7dd4a784deaca3e261caa1b3a48de7058c4"
   owner WS_USER
   action :install
 end
