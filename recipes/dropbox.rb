fcad8450d85cd3cc9426a50ab6bc08c699bc0ed3
Brian Cunnie <pair+cunnie@pivotallabs.com>
2013-03-14 15:01:48 -0700

    Dropbox 2.0.0

--- recipes/dropbox.rb
+++ recipes/dropbox.rb
@@ -1,7 +1,7 @@
 dmg_package "Dropbox" do
   volumes_dir "Dropbox Installer"
   source "https://www.dropbox.com/download?plat=mac"
-  checksum "e80f8974b1759f85f98c43c5ba8784f400ff9da507cf73077c2366c15bb04e33"
+  checksum "d74666cdb67ac7c82aedfaf8654bfc77c65fc9596d2a346ab08ec5c5cb4f735a"
   action :install
   owner WS_USER
 end
