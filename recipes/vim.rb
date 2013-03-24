3f3eafe21fd6c9d25fdd1cf0cccf36e2c513306f
Jacob Maine <pair+jacob@pivotallabs.com>
2013-03-24 00:52:47 -0400

    Split vim and vim_config recipes
    The vim_config recipe raises an error if the user is unwilling to cede
    control of the .vim directory.  Splitting the recipes lets those users
    avoid the error by excluding the vim_config recipe.  They can still
    install vim by including the vim recipe.

--- recipes/vim.rb
+++ recipes/vim.rb
@@ -2,9 +2,6 @@
 # https://github.com/Casecommons/casecommons_workstation
 
 include_recipe "pivotal_workstation::homebrew"
-include_recipe "pivotal_workstation::ack"
-include_recipe "pivotal_workstation::git"
-include_recipe "pivotal_workstation::tmux"
 
 execute "install macvim and use its vim as system vim" do
   command "brew install macvim --override-system-vim"
@@ -15,7 +12,7 @@ end
 execute "link macvim into place" do
   # Not using `brew linkapps` because that links to ~/Applications, not
   # /Applications.  If you decide linkapps is better, be sure to modify the
-  # "verify that command-t is correctly compiled" task.
+  # "verify that command-t is correctly compiled" task in the vim_config recipe.
   command "ln -s /usr/local/Cellar/macvim/*/MacVim.app /Applications/MacVim.app"
   user WS_USER
   not_if { File.symlink?("/Applications/MacVim.app") }
@@ -25,54 +22,4 @@ execute "test to see if macvim link worked" do
   command "test -L /Applications/MacVim.app"
 end
 
-ruby_block "ensure pivotal_workstation can manage #{node["vim_home"]}" do
-  missing = "test ! -d #{node["vim_home"]}"
-  present = "cd #{node["vim_home"]}"
-  owned_by_pw = "test -d .git && (git remote -v show | grep -q #{node["vim_config_git"]})"
-  not_if "#{missing} || (#{present} && #{owned_by_pw})"
-  block { raise "Rename or delete #{node["vim_home"]} if you want to use this recipe" }
-end
-
-git node["vim_home"] do
-  repository node["vim_config_git"]
-  branch "master"
-  revision node["vim_hash"] || "HEAD"
-  action :sync
-  user WS_USER
-  enable_submodules true
-end
-
-%w{vimrc gvimrc}.each do |vimrc|
-  link "#{WS_HOME}/.#{vimrc}" do
-    to "#{node["vim_home"]}/#{vimrc}"
-    owner WS_USER
-    not_if { File.symlink?("#{node["vim_home"]}/#{vimrc}") }
-  end
-end
-
-execute "compile command-t" do
-  # This matches the ruby that homebrew currently uses to `brew install macvim`
-  # See --with-ruby-command in https://github.com/mxcl/homebrew/blob/master/Library/Formula/macvim.rb
-  command "/System/Library/Frameworks/Ruby.framework/Versions/1.8/usr/bin/ruby extconf.rb && make clean && make"
-  cwd "#{node["vim_home"]}/bundle/command-t/ruby/command-t"
-  user WS_USER
-  only_if "test -d #{node["vim_home"]}/bundle/command-t/ruby/command-t"
-end
-
-execute "verify that command-t is correctly compiled for macvim" do
-  command <<-SH
-    findlibruby () { otool -l $1 | grep libruby; }
-    CMDT_RUBY=`findlibruby #{node["vim_home"]}/bundle/command-t/ruby/command-t/ext.bundle`
-    MACVIM_RUBY=`findlibruby /Applications/MacVim.app/Contents/MacOS/Vim`
-    test -n "$MACVIM_RUBY" -a "$MACVIM_RUBY" = "$CMDT_RUBY"
-  SH
-end
-
-file "#{WS_HOME}/.vimrc.local" do
-  action :touch
-  owner WS_USER
-  not_if { File.exists?("#{WS_HOME}/.vimrc.local") }
-end
-
 pivotal_workstation_bash_it_custom_plugin "vim-alias_vi_to_minimal_vim.bash"
-pivotal_workstation_bash_it_custom_plugin "vim-source_tmux_config.bash"
