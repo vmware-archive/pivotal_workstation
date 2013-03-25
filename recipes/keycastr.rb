da5f50a8bcf104af287daeb528e82aec0dc1505d
Brian Cunnie <pair+cunnie@pivotallabs.com>
2013-03-25 12:18:15 -0700

    Pivotal hosting keycastr; CNET changed URL

--- recipes/keycastr.rb
+++ recipes/keycastr.rb
@@ -5,7 +5,7 @@ app_path="/Applications/KeyCastr.app"
 unless File.exists?(app_path)
   dmg_package "KeyCastr" do
     dmg_name "KeyCastr"
-    source "http://software-files-a.cnet.com/s/software/11/00/84/53/keycastr_0.8.0.dmg"
+    source "http://cheffiles.pivotallabs.com/keycastr_0.8.0.dmg"
     checksum "ecf59a2e46a8ca450c12f426a4d41000de1c3010a5c46fe12f23fdc647f3f1e7"
     owner WS_USER
     action :install
