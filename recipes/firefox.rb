cb00895cf42236b349dd6ed551d12d65ed08ac63
Brian Cunnie <pair+cunnie@pivotallabs.com>
2013-04-05 14:27:20 -0700

    Firefox 20.0

--- recipes/firefox.rb
+++ recipes/firefox.rb
@@ -1,6 +1,6 @@
 dmg_package "Firefox" do
-  source "http://download.mozilla.org/?product=firefox-19.0&os=osx&lang=en-US"
-  checksum "4585e65043d5c7ea45534216706c15a1a2ae6fba0b457b83db550a5546b1422b"
+  source "http://download.mozilla.org/?product=firefox-20.0&os=osx&lang=en-US"
+  checksum "4848df9d4ce810bea0fc27ed4766bbfffe6afac184c601b07931da86b028720c"
   action :install
   owner WS_USER
 end
