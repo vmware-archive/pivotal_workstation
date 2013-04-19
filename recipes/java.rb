80e5e57e71d8d2685034b44c1c9ef317f6ee3841
Abhi Hiremagalur and Brian Cunnie <pair+abhi+cunnie@pivotallabs.com>
2013-04-19 12:42:08 -0700

    Update Java

--- recipes/java.rb
+++ recipes/java.rb
@@ -2,9 +2,9 @@ return unless node["platform"] == "mac_os_x"
 
 dmg_package "JavaForOSX" do
   source "http://support.apple.com/downloads/DL1572/en_US/"
-  volumes_dir "Java for OS X 2013-002"
+  volumes_dir "Java for OS X 2013-003"
   action :install
   type "pkg"
   package_id "com.apple.pkg.JavaForMacOSX107"
-  checksum "0ac0ab9bd1990add28bb1fa088fb8e59b657a09cbf5fde1c9d38eea8459760cc"
+  checksum "193daf9d16ba753cb7b9160e3b3842e515575a07ac2ec06d27c21a40c30c917b"
 end
